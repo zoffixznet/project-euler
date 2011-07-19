8e40e02e0bf7560098f039be8bc522fdf97fd360
Shlomi Fish <shlomif@iglu.org.il>
2011-07-19 15:52:18 +0300

    More progress, but not cigar.

--- project-euler/126/euler-126-take3.pl
+++ project-euler/126/euler-126-take3.pl
@@ -3,9 +3,13 @@
 use strict;
 use warnings;
 
+use IO::Handle;
+
 use integer;
 
 use List::MoreUtils qw(any all);
+
+STDOUT->autoflush(1);
 
 my @C;
 
@@ -29,47 +33,6 @@ sub add_layer
         ? (($x_lim*$y_lim+$x_lim*$z_lim+$z_lim*$y_lim)*2)
         : ($old_n+($x_lim+$y_lim+$z_lim)*4 + (($depth > 1) ? 8*($depth-2) : 0))
     );
-
-=begin foo
-    my $new_layer_count = 0;
-
-    my $calc_dist = sub {
-        my ($x, $x_lim) = @_;
-
-        return
-            (($x < 0) ? abs($x) : ($x >= $x_lim) ? ($x-($x_lim-1)) : 0);
-    };
-
-    foreach my $x (-$depth .. $x_lim+$depth-1)
-    {
-        my $dist_x = $calc_dist->($x,$x_lim);
-
-        foreach my $y (-$depth .. $y_lim + $depth-1)
-        {
-            my $dist_y = $calc_dist->($y,$y_lim);
-
-            foreach my $z (-$depth .. $z_lim + $depth-1)
-            {
-                my $dist_z = $calc_dist->($z, $z_lim);
-
-                if ($dist_x+$dist_y+$dist_z == $depth)
-                {
-                    $new_layer_count++;
-                }
-            }
-        }
-    }
-
-    if ($new_layer_count != $expected_new_layer_count)
-    {
-       die "\$new_layer_count != \$expected_new_layer_count ==
-        $new_layer_count != $expected_new_layer_count for depth $depth
-            and ($x_lim,$y_lim,$z_lim).";
-    }
-
-=end foo
-
-=cut
 
     $cuboids{$key} = {d => ($depth+1), n => $new_layer_count};
 
