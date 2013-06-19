7b4f8320a38cc401a24325a559d2027d58033f93
Shlomi Fish <shlomif@shlomifish.org>
2013-06-19 23:02:16 +0300

    Avoid memory leak.

--- project-euler/148/calc-num-Y-in-row-ORIG-2.pl
+++ project-euler/148/calc-num-Y-in-row-ORIG-2.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use 5.016;
+
 use integer;
 # use Math::BigInt lib => 'GMP', ':constant';
 
@@ -28,9 +30,7 @@ sub calc_num_Y_in_row_n
         $power *= $BASE;
     }
 
-    my $recurse;
-
-    $recurse = sub {
+    my $recurse = sub {
         my ($d_len) = @_;
 
         if ($d_len <= 0)
@@ -42,7 +42,7 @@ sub calc_num_Y_in_row_n
             my $big_Y_num = ($digits[$d_len]->{power}-1-$digits[$d_len-1]->{total_mod});
             my $big_Y_total = $big_Y_num * $digits[$d_len]->{d};
 
-            return $big_Y_total + ($digits[$d_len]->{d}+1) * $recurse->($d_len-1);
+            return $big_Y_total + ($digits[$d_len]->{d}+1) * __SUB__->($d_len-1);
         }
     };
 
