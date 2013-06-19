21e941cebd566711fd3fbfa63a2d9bca86a4baa6
Shlomi Fish <shlomif@shlomifish.org>
2013-06-19 19:15:31 +0300

    More.

---
+++ project-euler/148/calc-num-Y-in-row.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+sub fact
+{
+    return shift->copy->bfac;
+}
+
+sub nCr
+{
+    my ($n, $k) = @_;
+    $n += 0;
+    $k += 0;
+    return fact($n) / (fact($n-$k) * fact($k));
+}
+
+my $n = shift(@ARGV);
+
+print join(' ', map { +(nCr($n+1, $_) % 7 == 0 ) ? 'Y' : 'N' } (1 .. $n)), "\n";
