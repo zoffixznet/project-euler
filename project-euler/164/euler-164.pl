d4c46134262958915147ef5d3064aa459f141ec6
Shlomi Fish <shlomif@shlomifish.org>
2013-12-19 19:59:27 +0200

    Fixed. Working now

--- project-euler/164/euler-164.pl
+++ project-euler/164/euler-164.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 use Math::BigInt lib => 'GMP', ':constant';
-use List::MoreUtils qw(indexes);
+use List::MoreUtils qw(all indexes true);
 use List::Util qw(sum);
 
 my @counts;
@@ -34,9 +34,9 @@ sub get_count
 # Initialize $depth = 2;
 #
 my $INIT_DEPTH = 2;
-for my $ult (1 .. 9)
+for my $penult (1 .. 9)
 {
-    for my $penult (0 .. (9-$ult))
+    for my $ult (0 .. (9-$penult))
     {
         update_count($INIT_DEPTH, $penult, $ult, 1);
     }
@@ -51,6 +51,8 @@ for my $depth ($INIT_DEPTH+1 .. $MAX_DEPTH)
         {
             for my $new (0 .. (9-($penult+$ult)))
             {
+                # print "D=$depth $penult$ult$new\n";
+                # STDOUT->flush;
                 update_count($depth, $ult, $new,
                     get_count($depth, $penult, $ult)
                 );
@@ -60,4 +62,18 @@ for my $depth ($INIT_DEPTH+1 .. $MAX_DEPTH)
 
     my $sum = sum(map { $_ // 0 } map { @{$_ // []} } @{$counts[$depth]});
     print "Sum for depth=$depth = $sum\n";
+
+    if (0)
+    {
+        my $verify = sub {
+            my ($n) = @_;
+
+            return all { sum(split//,substr($n, $_, 3)) <= 9 } (0 .. (length($n)-3));
+        };
+        my $verify_sum =
+            true { $verify->("$_") }
+            ((10 ** ($depth-1)) .. (10 ** $depth - 1))
+            ;
+        print "VerifySum for depth=$depth = $verify_sum\n";
+    }
 }
