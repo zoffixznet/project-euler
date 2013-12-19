1ba7f71c10c326c6d6f69456824f50fd2b85b26c
Shlomi Fish <shlomif@shlomifish.org>
2013-12-19 16:44:53 +0200

    First version - not sure it will work.

---
+++ project-euler/162/euler-162.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+=begin foo
+
+In the hexadecimal number system numbers are represented using 16 different digits:
+0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+
+The hexadecimal number AF when written in the decimal number system equals 10x16+15=175.
+
+In the 3-digit hexadecimal numbers 10A, 1A0, A10, and A01 the digits 0,1 and A are all present.
+Like numbers written in base ten we write hexadecimal numbers without leading zeroes.
+
+How many hexadecimal numbers containing at most sixteen hexadecimal digits exist with all of the digits 0,1, and A present at least once?
+Give your answer as a hexadecimal number.
+
+(A,B,C,D,E and F in upper case, without any leading or trailing code that marks the number as hexadecimal and without leading zeroes , e.g. 1A3F and not: 1a3f and not 0x1a3f and not $1A3F and not #1A3F and not 0000001A3F)
+=end foo
+
+=cut
+
+# Let's assume without loss of generality that the leading (= most significant)
+# digit of the number is "1". Then we just tr/1A/A1/ and multiply by two.
+
+# Factorial
+sub fact
+{
+    my $i = shift;
+    return $i->copy->bfac;
+}
+
+my $sum = 0;
+for my $extra_len ((3-1) .. (16-1))
+{
+    print "extra_len == $extra_len\n";
+    for my $extra_num_1 (0 .. $extra_len-2)
+    {
+        my $num_A_0_others = $extra_len-$extra_num_1;
+        for my $num_A (1 .. $num_A_0_others-1)
+        {
+            my $num_0_and_others = $num_A_0_others - $num_A;
+            for my $num_0 (1 .. $num_0_and_others)
+            {
+                my $num_others = $num_0_and_others - $num_0;
+                $sum +=
+                (
+                (
+                    fact($extra_len) / ( fact($extra_num_1) * fact($num_A) * fact($num_0) * fact($num_others))
+                )
+                * ((16-3) ** $num_others)
+                );
+            }
+        }
+    }
+}
+
+$sum *= 2;
+print "Sum == $sum \nAs Hex: ", (uc($sum->as_hex) =~ s/\A0x//r), "\n";
