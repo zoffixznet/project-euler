b651686507a3a504a07166e3bbbaa6979afa5deb
Shlomi Fish <shlomif@shlomifish.org>
2014-03-14 14:44:36 +0200

    Enhance and correct the Euler #455 program.

--- project-euler/455/euler-455-v1.pl
+++ project-euler/455/euler-455-v1.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use autodie;
 
 use 5.016;
 
@@ -24,6 +25,112 @@ Find Σf(n), 2 ≤ n ≤ 106.
 =cut
 
 my $MOD = 1_000_000_000;
+
+sub exp_mod
+{
+    my ($b, $e) = @_;
+
+    if ($e == 0)
+    {
+        return 1;
+    }
+
+    my $rec_p = exp_mod($b, ($e >> 1));
+
+    my $ret = $rec_p * $rec_p;
+
+    if ($e & 0x1)
+    {
+        $ret *= $b;
+    }
+
+    return ($ret % $MOD);
+}
+
+# my $START = $MOD-1;
+my $START = 999997951;
+my $SEGMENT = 1024;
+
+my $DUMP_FN =  'euler-455.txt';
+
+my $sum = 0;
+# Every n^e where e > 2 will have more zeros than needed.
+my @n_s = (grep { $_ % 10 != 0 } (2 .. 1_000_000));
+
+{
+    my %blacklist;
+    open my $in, '<', $DUMP_FN;
+    while (my $l = <$in>)
+    {
+        chomp($l);
+        if (my ($x, $n) = $l =~ /\AFound x=(\d+) for n=(\d+)\z/)
+        {
+            $sum += $x;
+            $blacklist{$n} = 1;
+        }
+        elsif (my ($reached) = $l =~ /\AInspecting (\d+)/)
+        {
+            $START = $reached;
+        }
+    }
+    close($in);
+
+    @n_s = grep { !exists($blacklist{$_}) } @n_s;
+}
+
+STDOUT->autoflush(1);
+
+my $range_top = $START;
+
+open my $out_fh, '>>', $DUMP_FN;
+$out_fh->autoflush(1);
+while ($range_top > $SEGMENT)
+{
+    say {$out_fh} "Inspecting $range_top";
+    my $bottom = $range_top - ($SEGMENT - 1);
+
+    my @next_n_s;
+    for my $n (@n_s)
+    {
+        my $e = $bottom;
+        my $m = exp_mod($n, $e);
+        my $found_e;
+        while ($e <= $range_top)
+        {
+            if ($m == $e)
+            {
+                $found_e = $e;
+            }
+            $m = (($m * $n) % $MOD);
+        }
+        continue
+        {
+            $e++;
+        }
+
+        if (defined($found_e))
+        {
+            say {$out_fh} "Found x=$found_e for n=$n";
+            $sum += $found_e;
+        }
+        else
+        {
+            push @next_n_s, $n;
+        }
+    }
+    @n_s = @next_n_s;
+}
+continue
+{
+    $range_top -= $SEGMENT;
+}
+close($out_fh);
+say "Sum == $sum";
+
+
+__END__
+
+=begin FOO
 
 sub func
 {
@@ -65,4 +172,6 @@ sub func
     }
 }
 
-say "f(4) == ", func(4);
+=end FOO
+
+=cut
