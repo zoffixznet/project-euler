750735b27efd43ee239195481f6c288161205c0c
Shlomi Fish <shlomif@shlomifish.org>
2014-03-15 12:22:17 +0200

    Optimised the memory consumption.

--- project-euler/455/euler-455-v2.pl
+++ project-euler/455/euler-455-v2.pl
@@ -56,14 +56,29 @@ sub calc_f
         return 0;
     }
 
-    my %cache;
+    my $cache = '';
+
+    my $lookup = sub {
+        my ($power) = @_;
+
+        return vec($cache, $MOD-$power, 32);
+    };
+
+    my $assign = sub {
+        my ($power, $e) = @_;
+
+        vec($cache, $MOD-$power, 32) = $e;
+
+        return;
+    };
 
     my $e = 0;
     my $power = 1;
 
-    while (! exists($cache{$power}))
+    # Find the first cycle len.
+    while (! vec($cache, $power, 1))
     {
-        $cache{$power} = $e;
+        vec($cache,$power, 1) = 1;
     }
     continue
     {
@@ -71,24 +86,46 @@ sub calc_f
         ($power *= $n) %= $MOD;
     }
 
-    my $cycle_len = $e-$cache{$power};
+    my $found_power = $power;
+    my $found_e = $e;
 
-    # Inspect the largest ones first.
-    for my $x (sort { $b <=> $a } keys%cache)
+    $power = 1;
+    $e = 0;
+    while ($power != $found_power)
     {
-        if (( ($x - $cache{$x}) % $cycle_len) == 0)
-        {
-            return $x;
-        }
+        $e++;
+        ($power *= $n) %= $MOD;
     }
 
-    return 0;
+    my $cycle_len = $found_e-$e;
+
+    $power = 1;
+    my $x = 0;
+    for my $e2 (0 .. $found_e)
+    {
+        if (($power - $e2) % $cycle_len == 0
+                and $power > $x
+        )
+        {
+            $x = $power;
+        }
+    }
+    continue
+    {
+        ($power *= $n) %= $MOD;
+    }
+
+    print "f($n) == $x\n";
+
+    return $x;
 }
 
 my $MAX = 1_000_000;
 my $sum = 0;
 
 my ($START, $END) = @ARGV;
+
+STDOUT->autoflush(1);
 
 foreach my $n ($START .. $END)
 {
