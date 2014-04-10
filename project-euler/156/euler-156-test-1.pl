085fa2c84d23c0f2f69d031b5942430867ee8a84
Shlomi Fish <shlomif@shlomifish.org>
2014-04-10 13:34:26 +0300

    Add more to Euler #156.

---
+++ project-euler/156/euler-156-test-1.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+my $last_delta_f = 0;
+for my $n (1 .. 100)
+{
+    my $next_delta_f = $last_delta_f + ($last_delta_f * 9 + (10 ** ($n-1)));
+    printf "In 0 - %30s delta_f = %30s\n", 10 ** $n - 1, $next_delta_f;
+    $last_delta_f = $next_delta_f;
+}
