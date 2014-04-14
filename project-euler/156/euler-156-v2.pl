a2427697aee42ce1896cee7e85ccf2fc0c2c33ac
Shlomi Fish <shlomif@shlomifish.org>
2014-04-14 15:04:53 +0300

    Add f_d_n.

--- project-euler/156/euler-156-v2.pl
+++ project-euler/156/euler-156-v2.pl
@@ -8,7 +8,7 @@ use Math::BigInt lib => 'GMP', ':constant';
 use List::Util qw(sum);
 use List::MoreUtils qw();
 
-use Test::More tests => 8;
+use Test::More tests => 9;
 
 sub calc_f_delta
 {
@@ -21,7 +21,41 @@ sub calc_f_delta_for_leading_digits
 {
     my ($num_digits_after, $num_leading_d_digits) = @_;
 
-    return $num_leading_d_digits * 10 ** $num_digits_after + calc_f_delta($num_digits_after-1);
+    return $num_leading_d_digits * 10 ** $num_digits_after + (($num_digits_after-1 >= 0) ? calc_f_delta($num_digits_after-1) : 0);
+}
+
+sub f_d_n
+{
+    my ($d, $n) = @_;
+
+    my @digits = split(//, ($n+1).'');
+
+    my $ret = 0;
+
+    my $num_leading_d_digits = 0;
+
+    foreach my $place (reverse(keys @digits))
+    {
+        my $place_d = $digits[$place];
+
+        for my $iter_d (0 .. $place_d-1)
+        {
+            if ($iter_d != $d)
+            {
+                $ret += calc_f_delta_for_leading_digits($place, $num_leading_d_digits);
+            }
+            else
+            {
+                $ret += calc_f_delta_for_leading_digits($place, $num_leading_d_digits+1);
+            }
+        }
+        if ($place_d == $d)
+        {
+            $num_leading_d_digits++;
+        }
+    }
+
+    return $ret;
 }
 
 # TEST
@@ -78,4 +112,11 @@ is (
     calc_f_delta_for_leading_digits(1, 2),
     21,
     "calc_f_delta_for_leading_digits(1,2)",
+);
+
+# TEST
+is (
+    f_d_n(1, 1),
+    1,
+    "f_d_n(1,1)",
 );
