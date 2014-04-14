78e63d491d4690b872e25dc7ac15e94e6b069e61
Shlomi Fish <shlomif@shlomifish.org>
2014-04-14 15:10:08 +0300

    Split into a test file and a module.

---
+++ project-euler/156/Euler156_V2.pm
@@ -0,0 +1,63 @@
+package Euler156_V2;
+
+use strict;
+use warnings;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+use parent 'Exporter';
+
+our @EXPORT_OK = qw(calc_f_delta_for_leading_digits calc_f_delta f_d_n);
+
+sub calc_f_delta
+{
+    my ($exp) = @_;
+
+    return ($exp+1) * (10 ** $exp);
+}
+
+sub calc_f_delta_for_leading_digits
+{
+    my ($num_digits_after, $num_leading_d_digits) = @_;
+
+    return $num_leading_d_digits * 10 ** $num_digits_after + (($num_digits_after-1 >= 0) ? calc_f_delta($num_digits_after-1) : 0);
+}
+
+sub f_d_n
+{
+    my ($d, $n) = @_;
+
+    my @digits = split(//, ($n+1).'');
+
+    my $ret = 0;
+
+    my $num_leading_d_digits = 0;
+
+    foreach my $place (reverse(keys @digits))
+    {
+        my $place_d = $digits[$place];
+
+        for my $iter_d (0 .. $place_d-1)
+        {
+            if ($iter_d != $d)
+            {
+                $ret += calc_f_delta_for_leading_digits($place, $num_leading_d_digits);
+            }
+            else
+            {
+                $ret += calc_f_delta_for_leading_digits($place, $num_leading_d_digits+1);
+            }
+        }
+        if ($place_d == $d)
+        {
+            $num_leading_d_digits++;
+        }
+    }
+
+    return $ret;
+}
+
+1;
---
+++ project-euler/156/euler-156-v2.t
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+use Test::More tests => 10;
+
+use Euler156_V2 qw(calc_f_delta_for_leading_digits calc_f_delta f_d_n);
+
+# TEST
+is (
+    calc_f_delta(0),
+    1,
+    "calc_f_delta(0)",
+);
+
+# TEST
+is (
+    calc_f_delta(1),
+    20,
+    "calc_f_delta(1)",
+);
+
+# TEST
+is (
+    calc_f_delta(2),
+    300,
+    "calc_f_delta(2)",
+);
+
+# TEST
+is (
+    calc_f_delta(8),
+    900000000,
+    "calc_f_delta(8)",
+);
+
+# TEST
+is (
+    calc_f_delta_for_leading_digits(1, 0),
+    1,
+    "calc_f_delta_for_leading_digits(1,0)",
+);
+
+# TEST
+is (
+    calc_f_delta_for_leading_digits(2, 0),
+    20,
+    "calc_f_delta_for_leading_digits(2,0)",
+);
+
+# TEST
+is (
+    calc_f_delta_for_leading_digits(1, 1),
+    11,
+    "calc_f_delta_for_leading_digits(1,1)",
+);
+
+# TEST
+is (
+    calc_f_delta_for_leading_digits(1, 2),
+    21,
+    "calc_f_delta_for_leading_digits(1,2)",
+);
+
+# TEST
+is (
+    f_d_n(1, 1),
+    1,
+    "f_d_n(1,1)",
+);
+
+# TEST
+is (
+    f_d_n(1, 2),
+    1,
+    "f_d_n(1,2)",
+);
