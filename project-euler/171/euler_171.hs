de9ddf5483601af566f1f7a5038ae245d996a6ef
Shlomi Fish <shlomif@shlomifish.org>
2014-04-20 19:27:43 +0300

    Add the combos function.

--- project-euler/171/euler_171.hs
+++ project-euler/171/euler_171.hs
@@ -1,2 +1,6 @@
-sums :: [Int]
-sums = takeWhile (\x -> x < 9*9*20) [x*x | x <- [1..]]
+sums :: [Integer]
+sums = takeWhile (\x -> x <= 9*9*20) [x*x | x <- [1..]]
+
+combos :: Integer -> Integer -> Integer -> [[Integer]]
+combos 0 sum start = if (sum > 0) then [] else [[]]
+combos len sum start = [(x:xs) | x <- filter (\x -> sum - x * x - 9*9*(len-1) <= 0) (takeWhile (\x -> x*x <= sum) [start .. 9]), xs <- combos (len-1) (sum - x*x) x]
