6fb708aa5c2cc849285c55c3510dc466cbcfc268
Shlomi Fish <shlomif@shlomifish.org>
2014-05-03 14:11:15 +0300

    Add more.

--- project-euler/165/euler_165.hs
+++ project-euler/165/euler_165.hs
@@ -1,4 +1,5 @@
 import Data.Int
+import Data.List
 
 r_transform :: Int64 -> Int64
 
@@ -16,3 +17,31 @@ get_seg :: [Int64] -> [Seg]
 get_seg (a:b:c:d:rest) = (a,b,c,d):(get_seg rest)
 
 segs = get_seg get_t
+
+data Frac = Frac {
+      frac_n :: Int64
+    , frac_d :: Int64
+} deriving (Show)
+
+data Type_X_Only_Seg = Type_X_Only_Seg {
+      x_Only_x :: Int64
+    , x_Only_y1 :: Int64
+    , x_Only_y2 :: Int64
+} deriving (Show)
+
+data Type_XY_Seg = Type_XY_Seg {
+      xy_m :: Frac
+    , xy_b :: Frac
+    , xy_x1 :: Int64
+    , xy_x2 :: Int64
+} deriving (Show)
+
+data CompiledSeg = CompX Type_X_Only_Seg
+                 | CompXY Type_XY_Seg
+
+compile_segment :: Seg -> CompiledSeg
+compile_segment (x1,y1,x2,y2) = if (x1 == x2)
+    then let y_s = (sort [y1,y2]) in (CompX $ Type_X_Only_Seg x1 (y_s!!0) (y_s!!1))
+    else (CompX $ Type_X_Only_Seg x1 0 0)
+
+mysegs = map compile_segment $ take 5000 segs
