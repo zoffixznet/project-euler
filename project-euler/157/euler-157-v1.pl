a8c6035e67f4e9580a64538168b716151b28712b
Shlomi Fish <shlomif@shlomifish.org>
2014-05-11 12:38:55 +0300

    Euler #157: initial version.
    Work only for $n = 1 and does not terminate.

---
+++ project-euler/157/euler-157-v1.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+# use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+for my $n (1)
+{
+    my $t_n = (10 ** $n);
+    my $B = 1;
+    while (1)
+    {
+        for my $A (1 .. $B)
+        {
+            my $num = ($A+$B)*$t_n;
+            my $denom = $A * $B;
+            if ($num % $denom == 0)
+            {
+                print "Found 1/$A+1/$B = " . ($num/$denom) . "/$t_n\n";
+            }
+        }
+    }
+    continue
+    {
+        $B++;
+    }
+}
