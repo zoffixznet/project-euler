0ebf59482faa91909e50372ffb1c24868fea0604
Shlomi Fish <shlomif@shlomifish.org>
2014-11-07 16:01:17 +0200

    Optimisations - move away from Math::BigRat.

--- project-euler/180/euler-180.pl
+++ project-euler/180/euler-180.pl
@@ -36,49 +36,80 @@ foreach my $n (1 .. ($K*2))
 }
 
 my %found_s;
-my $total_sum = Math::BigRat->new("0/1");
+my @total_sum = (0,1);
+
+sub gcd
+{
+    my ($n, $m) = @_;
+
+    if ($n < $m)
+    {
+        return gcd($m,$n);
+    }
+
+    if ($m == 0)
+    {
+        return $n;
+    }
+
+    return gcd($m,$n % $m);
+}
+
+sub norm
+{
+    my ($aa, $bb) = @_;
+    my $g = gcd($aa, $bb);
+
+    return ($aa / $g, $bb / $g);
+}
+
+sub add
+{
+    my ($x_a, $x_b, $y_a, $y_b) = @_;
+
+    return norm ($x_a*$y_b+$x_b*$y_a,$x_b*$y_b);
+}
 
 sub check
 {
-    my ($n, $x_a, $x_b, $y_a, $y_b) = @_;
+    my $n = shift;
+    my @xy = @_;
+    my ($x_a, $x_b, $y_a, $y_b) = @_;
 
-    my $x = Math::BigRat->new("$x_a/$x_b");
-    my $y = Math::BigRat->new("$y_a/$y_b");
-    my $sum = $x ** $n + $y ** $n;
+    # my $x = Math::BigRat->new("$x_a/$x_b");
+    # my $y = Math::BigRat->new("$y_a/$y_b");
+    my @sum = add(($n == 1) ? @xy : map { $_ * $_ } @xy);
 
-    $sum->bnorm();
-
-    my $z;
+    my @z;
     if ($n == 2)
     {
-        my $z_a = $sqrts{$sum->numerator};
-        my $z_b = $sqrts{$sum->denominator};
+        my $z_a = $sqrts{$sum[0]};
+        my $z_b = $sqrts{$sum[1]};
 
-        if (!defined($z_a) or (!defined($z_b)))
+        if ((!defined($z_a)) or (!defined($z_b)))
         {
             return;
         }
-        $z = Math::BigRat->new("$z_a/$z_b");
+        @z = ($z_a, $z_b);
     }
     else
     {
-        $z = $sum;
+        @z = @sum;
     }
 
     {
         if
         (
             ($x_a < $x_b)
-            ? ($z->numerator < $z->denominator and $z->denominator <= $K)
-            : ($z->numerator > $z->denominator and $z->numerator <= $K)
+            ? ($z[0] < $z[1] and $z[1] <= $K)
+            : ($z[0] > $z[1] and $z[0] <= $K)
         )
         {
-            my $s = $x + $y + $z;
-            $s->bnorm;
-            my $s_str = $s->bstr;
+            my @s = add(add(@xy), @z);
+            my $s_str = "$s[0]/$s[1]";
             if (!exists($found_s{$s_str}))
             {
-                $total_sum += $s;
+                @total_sum = add(@s, @total_sum);
                 $found_s{$s_str} = 1;
             }
         }
@@ -86,22 +117,25 @@ sub check
 
 }
 
-pick_frac(
-    sub {
-        my ($x_a, $x_b) = @_;
-        pick_frac(
-            sub {
-                my ($y_a, $y_b) = @_;
-                print "{$x_a,$x_b,$y_a,$y_b}\n";
+for my $x_b (2 .. $K)
+{
+    print "x_b=$x_b\n";
+    for my $x_a (1 .. $x_b - 1)
+    {
+        # We can assume without loss of generality that y >= x.
+        for my $y_b ($x_b .. $K)
+        {
+            for my $y_a (1 .. $y_b - 1)
+            {
 
                 for my $n (1, 2)
                 {
                     check($n, $x_a, $x_b, $y_a, $y_b);
                     check($n, $x_b, $x_a, $y_b, $y_a);
                 }
-            },
-        );
-    },
-);
+            }   
+        }
+    }
+}
 
-print "$total_sum\n";
+print "$total_sum[0]/$total_sum[1] == @{[$total_sum[0]+$total_sum[1]]}\n";
