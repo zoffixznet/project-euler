1cb8d5e5868c934495547d7459da4db3abe2cbad
Shlomi Fish <shlomif@shlomifish.org>
2014-11-07 15:38:02 +0200

    add an incorrect solution to Euler #180.

--- project-euler/180/euler-180.pl
+++ project-euler/180/euler-180.pl
@@ -3,40 +3,105 @@
 use strict;
 use warnings;
 
-use integer;
 use bytes;
 
-use Math::BigInt lib => 'GMP', ':constant';
+use Math::BigRat lib => 'GMP';
 
 use List::Util qw(sum);
 use List::MoreUtils qw();
 
 STDOUT->autoflush(1);
 
-sub check
-{
-    my ($n, $x, $y, $z) = (map { Math::BigInt->new("$_") } @_);
+my $K = 35;
 
-    if ($x ** ($n+1) + $y ** ($n+1) - $z ** ($n+1) + ($x*$y+$y*$z+$z*$x) * ($x ** ($n-1) + $y ** ($n-1) - $z ** ($n-1)) - $x * $y * $z * ($x ** ($n-2) + $y ** ($n-2) - $z ** ($n-2)) != (($x+$y+$z)*($x**$n + $y**$n - $z**$n)))
+sub pick_frac
+{
+    my ($cb) = @_;
+
+    for my $bb ( 2 .. $K)
     {
-        print "$x,$y,$z\[n=$n] Mismatch.\n";
-        die "Foo";
+        for my $aa (1 .. $bb-1)
+        {
+            $cb->($aa, $bb);
+        }
     }
 
     return;
 }
 
-for my $z (1 .. 100)
+my %sqrts;
+foreach my $n (1 .. ($K*2))
 {
-    for my $x (1 .. $z)
+    $sqrts{$n*$n} = $n;
+}
+
+my %found_s;
+my $total_sum = Math::BigRat->new("0/1");
+
+sub check
+{
+    my ($n, $x_a, $x_b, $y_a, $y_b) = @_;
+
+    my $x = Math::BigRat->new("$x_a/$x_b");
+    my $y = Math::BigRat->new("$y_a/$y_b");
+    my $sum = $x ** $n + $y ** $n;
+
+    $sum->bnorm();
+
+    my $z;
+    if ($n == 2)
     {
-        for my $y (1 .. $x)
+        my $z_a = $sqrts{$sum->numerator};
+        my $z_b = $sqrts{$sum->denominator};
+
+        if (!defined($z_a) or (!defined($z_b)))
         {
-            for my $n (3 .. 10)
+            return;
+        }
+        $z = Math::BigRat->new("$z_a/$z_b");
+    }
+    else
+    {
+        $z = $sum;
+    }
+
+    {
+        if
+        (
+            ($x_a < $x_b)
+            ? ($z->numerator < $z->denominator and $z->denominator <= $K)
+            : ($z->numerator > $z->denominator and $z->numerator <= $K)
+        )
+        {
+            my $s = $x + $y + $z;
+            $s->bnorm;
+            my $s_str = $s->bstr;
+            if (!exists($found_s{$s_str}))
             {
-                print "Checking $x,$y,$z,$n\n";
-                check ($n,$x,$y,$z);
+                $total_sum += $s;
+                $found_s{$s_str} = 1;
             }
         }
     }
+
 }
+
+pick_frac(
+    sub {
+        my ($x_a, $x_b) = @_;
+        pick_frac(
+            sub {
+                my ($y_a, $y_b) = @_;
+                print "{$x_a,$x_b,$y_a,$y_b}\n";
+
+                for my $n (1, 2)
+                {
+                    check($n, $x_a, $x_b, $y_a, $y_b);
+                    check($n, $x_b, $x_a, $y_b, $y_a);
+                }
+            },
+        );
+    },
+);
+
+print "$total_sum\n";
