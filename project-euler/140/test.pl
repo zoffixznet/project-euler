dfd0cb0b260a050cd36e6c27cbd468084a01bdd2
Shlomi Fish <shlomif@shlomifish.org>
2014-11-08 12:06:55 +0200

    add more.

---
+++ project-euler/140/test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use 5.018;
+
+sub _calc
+{
+    my ($i) = @_;
+
+    my $n = $i;
+    my $s = sqrt(5);
+
+    my $A = +(1+$s)/2;
+    my $B = +(1-$s)/2;
+
+    return $A**$n*(4/$s*$A+1/$s)+$B**$n*(-4*$B/$s-1/$s);
+}
+
+say _calc(shift(@ARGV));
