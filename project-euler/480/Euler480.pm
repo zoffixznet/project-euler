b40757f644f643464e3dd0a3fc000e85eaef5992
Shlomi Fish <shlomif@shlomifish.org>
2014-11-29 20:31:46 +0200

    Add more.

--- project-euler/480/Euler480.pm
+++ project-euler/480/Euler480.pm
@@ -26,11 +26,63 @@ foreach my $l (split//,$WORD)
 
 my @LETTERS = (map { [$_,$MAP{$_}] } sort keys %MAP);
 
+my @WEIGHTS_PROTO = (reverse sort { $a <=> $b } map { $_->[1] } @LETTERS);
+
+my @WEIGHTS;
+
+{
+    my $start_idx = 0;
+    foreach my $i (1 .. $#WEIGHTS_PROTO)
+    {
+        if ($WEIGHTS_PROTO[$i] != $WEIGHTS_PROTO[$start_idx])
+        {
+            push @WEIGHTS, [$WEIGHTS_PROTO[$start_idx], $i - $start_idx];
+            $start_idx = $i;
+        }
+    }
+    push @WEIGHTS, [$WEIGHTS_PROTO[$start_idx], $#WEIGHTS_PROTO - $start_idx];
+}
+
+my @caches_by_len;
+
 sub calc_num_words_with_letters
 {
-    my ($letters, $max_len) = @_;
+    my ($weights, $max_len) = @_;
 
-    my $
+    if ($max_len == 0)
+    {
+        return 1;
+    }
+
+    return $caches_by_len[$max_len]{join(',',
+            map { ($_->[0]) x $_->[1] } @$weights
+        )
+    } //= do {
+        my $ret = 1;  # For the zero length string.
+
+        my @new_weights;
+        for my $w_idx (keys (@$weights))
+        {
+            my ($w, $count) = @{ $weights->[$w_idx] };
+            $ret += $count * calc_num_words_with_letters(
+                [@new_weights,
+                    (($count > 1) ? ([$w, $count-1]) : ()),
+                    (($w_idx < $#$weights)
+                        ? (($weights->[$w_idx+1]->[0] == $w-1)
+                            ? ([$w-1, $weights->[$w_idx+1]->[1]+1])
+                            : ([$w-1, 1], $weights->[$w_idx+1])
+                        )
+                        : ($w > 1 ? ([$w-1, 1]) : ())
+                    ),
+                    @{$weights}[$w_idx+2 .. $#$weights]
+                ],
+                $max_len - 1
+            );
+            push @new_weights, $weights->[$w_idx];
+        }
+
+        $ret;
+    };
 }
 
 sub calc_W
