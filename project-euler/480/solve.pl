7e1470a7dba59a02254f9e772d05a465d7dd7b6f
Shlomi Fish <shlomif@shlomifish.org>
2014-11-30 13:09:37 +0200

    Add the correct solution.

---
+++ project-euler/480/solve.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Euler480;
+
+sub W
+{
+    return Euler480::calc_W(@_);
+}
+
+sub P
+{
+    return Euler480::calc_P(@_);
+}
+print "Result = <<", W(P('legionary') + P('calorimeters') - P('annihilate') + P('orchestrated') - P('fluttering')), ">>\n";
