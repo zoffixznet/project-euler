606b9122a097e00f08512945ea06b06c37ea63e1
Shlomi Fish <shlomif@shlomifish.org>
2015-02-02 17:24:05 +0200

    Completed the program, but it's too slow.

--- project-euler/209/euler-209-v2.pl
+++ project-euler/209/euler-209-v2.pl
@@ -6,8 +6,10 @@ use warnings;
 use integer;
 use bytes;
 
+use Data::Dumper;
+
 use List::Util qw(sum);
-use List::MoreUtils qw(none);
+use List::MoreUtils qw(any none);
 
 STDOUT->autoflush(1);
 
@@ -29,6 +31,7 @@ sub recurse
 {
     my ($queue, $state) = @_;
 
+    print "Q==@$queue\n";
     if (!@$queue)
     {
         return 1;
@@ -50,4 +53,45 @@ sub recurse
     return $ret;
 }
 
-# Find Fully-connected components.
+# Find Fully-connected components (FCCs).
+
+my @FCCs = ([]);
+
+my %to_traverse = (map { $_ => 1 } keys @Graph);
+
+foreach my $inputs (0 .. (2**6-1))
+{
+    my @q = ($inputs);
+    while (defined  ( my $i = shift(@q)) )
+    {
+        if (exists($to_traverse{$i}))
+        {
+            delete $to_traverse{$i};
+            push @{$FCCs[-1]}, $i;
+            push @q, @{$Graph[$i]};
+        }
+    }
+
+    if (@{$FCCs[-1]})
+    {
+        $FCCs[-1] = [ sort { $a <=> $b } @{$FCCs[-1]} ];
+        push @FCCs, []
+    }
+}
+
+if (! @{$FCCs[-1]})
+{
+    pop(@FCCs);
+}
+
+my $result = 1;
+
+foreach my $fcc (@FCCs)
+{
+    my @state = ((-1) x 64);
+    $state[63] = 0;
+    $result *= recurse([grep { $_ != 63 } @$fcc], \@state);
+}
+
+print "Result == $result\n";
+# print Dumper ([ \@FCCs ] )
