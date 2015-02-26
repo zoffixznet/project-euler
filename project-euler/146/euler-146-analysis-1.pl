ed07e8af28b6bf5082804bb38681fe1b10a98384
Shlomi Fish <shlomif@shlomifish.org>
2015-02-26 14:55:35 +0200

    Add what we have of #146 for now.

---
+++ project-euler/146/euler-146-analysis-1.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use autodie;
+
+# use integer;
+use bytes;
+
+# use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw(any none);
+
+STDOUT->autoflush(1);
+
+my $prod = 1;
+
+my @primes;
+
+open (my $list_fh, "primes 2|");
+
+P:
+while (my $p = <$list_fh>)
+{
+    chomp($p);
+    $p = 0 + $p;
+    push @primes, $p;
+    $prod *= $p;
+
+    my @modulos;
+
+    for my $mod (0 .. $prod-1)
+    {
+        my $sq_mod = $mod*$mod;
+        if (none { my $p = $_; any { ($sq_mod + $_) % $p == 0 } (1,3,7,9,13,27)} @primes)
+        {
+            push @modulos, $mod;
+        }
+    }
+
+    print "For $prod: " . sprintf("%.100f", ((@modulos + 0.0) / $prod)) . "[@modulos]\n";
+
+    if ($prod > 9_000_000)
+    {
+        last P;
+    }
+}
