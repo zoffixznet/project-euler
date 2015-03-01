d90c9ffcb376c1cb0f6e03cfab85e42e53a88e2f
Shlomi Fish <shlomif@shlomifish.org>
2015-03-01 10:38:00 +0200

    Remove fluff.

--- project-euler/14.pl
+++ project-euler/14.pl
@@ -6,16 +6,12 @@ use warnings;
 no warnings 'recursion';
 
 use integer;
-use bytes;
 
-use List::Util qw(sum);
-use List::MoreUtils qw();
 use List::UtilsBy qw(max_by);
-
-STDOUT->autoflush(1);
 
 {
     my %c = (1 => 0);
+
 sub calc_seq_rank
 {
     my ($n) = @_;
@@ -24,6 +20,7 @@ sub calc_seq_rank
         (1 + calc_seq_rank(($n & 1) ? ($n*3+1) : ($n >> 1)))
     );
 }
+
 }
 
 print "N = " , ( max_by { calc_seq_rank($_)} 2 .. 999_999), "\n";
