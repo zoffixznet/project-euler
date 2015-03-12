1e97522b0d10dec5ab40289be7bf2c8727a54e99
Shlomi Fish <shlomif@shlomifish.org>
2015-03-12 17:21:49 +0200

    Add the solution.
    It yields a correct solution but takes quite a while.

---
+++ project-euler/196/euler-196-v1-solution.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Row;
+
+sub S
+{
+    my ($i) = @_;
+
+    my $row = Row->new({idx => $i});
+    $row->mark_primes;
+
+    return $row->calc_S;
+}
+
+print "The solution is ", S(5678027) + S(7208785), "\n";
