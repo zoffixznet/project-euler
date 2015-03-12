2263f86e1825c92dbf4b44729e1f2561a7707816
Shlomi Fish <shlomif@shlomifish.org>
2015-03-12 15:56:17 +0200

    Add ->end.

--- project-euler/196/euler-196-v1.pl
+++ project-euler/196/euler-196-v1.pl
@@ -16,9 +16,15 @@ has 'start' => (isa => 'Int', is => 'ro', lazy => 1, default => sub {
     }
 );
 
+has 'end' => (isa => 'Int', is => 'ro', lazy => 1, default => sub {
+        my $self = shift;
+        return $self->start + $self->idx - 1;
+    }
+);
+
 package main;
 
-use Test::More tests => 4;
+use Test::More tests => 8;
 
 # TEST
 is (Row->new({idx => 1})->start(), 1, "Row[1].start");
@@ -31,3 +37,15 @@ is (Row->new({idx => 3})->start(), 4, "Row[3].start");
 
 # TEST
 is (Row->new({idx => 4})->start(), 7, "Row[4].start");
+
+# TEST
+is (Row->new({idx => 1})->end(), 1, "Row[1].end");
+
+# TEST
+is (Row->new({idx => 2})->end(), 3, "Row[2].end");
+
+# TEST
+is (Row->new({idx => 3})->end(), 6, "Row[3].end");
+
+# TEST
+is (Row->new({idx => 4})->end(), 10, "Row[4].end");
