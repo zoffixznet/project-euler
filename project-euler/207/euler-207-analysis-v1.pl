0bb51d4db2568bd548816505004382ea6ff6642f
Shlomi Fish <shlomif@shlomifish.org>
2015-03-27 19:44:06 +0300

    Fix some bugs , but still the wrong solution.

--- project-euler/207/euler-207-analysis-v1.pl
+++ project-euler/207/euler-207-analysis-v1.pl
@@ -3,13 +3,30 @@
 use strict;
 use warnings;
 
-my $n = 4;
+use integer;
+use bytes;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my $n = 3;
 my $num_powers = 2;
+
+sub cond
+{
+    return (($num_powers-1) * 12345 <= ($n-1));
+    # return (($num_powers-1) * 13 < ($n-1) * 3);
+    # return (($num_powers-1) * 5 < ($n-1) * 2);
+}
 
 MAIN:
 while (1)
 {
-    if (($num_powers-1) / ($n-2) < 1/12345)
+    if (cond())
     {
         print "For \$num_powers=$num_powers ; \$n=$n\n";
         last MAIN;
@@ -18,16 +35,16 @@ while (1)
 continue
 {
     $n *= 2;
+    $n++;
     $num_powers++;
 }
 
-while (($num_powers-1) / ($n-2) < 1/12345)
+while (cond())
 {
     $n--;
 }
 
 $n++;
-$n++;
 
-print "N=$n\n";
+print "N = $n\n";
 print "k = ", ($n*$n-$n), "\n";
