a95ce9175e864730c828c955bc63c4aa20e360ad
Shlomi Fish <shlomif@shlomifish.org>
2015-03-31 15:37:56 +0300

    Bug fix - now it's working.

--- project-euler/207/euler-207-analysis-v1.pl
+++ project-euler/207/euler-207-analysis-v1.pl
@@ -13,12 +13,16 @@ use List::MoreUtils qw();
 
 STDOUT->autoflush(1);
 
+my $frac = shift(@ARGV) || '1/12345';
+
+my ($numer, $denom) = split/\//, $frac;
+
 my $n = 3;
 my $num_powers = 2;
 
 sub cond
 {
-    return (($num_powers-1) * 12345 <= ($n-1));
+    return (($num_powers-1) * $denom < ($n-1) * $numer);
     # return (($num_powers-1) * 13 < ($n-1) * 3);
     # return (($num_powers-1) * 5 < ($n-1) * 2);
 }
