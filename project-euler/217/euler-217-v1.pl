133a20f10b20edbc58a0edf28c896296487e8166
Shlomi Fish <shlomif@shlomifish.org>
2015-04-11 12:17:49 +0300

    Correctly solve #217.

--- project-euler/217/euler-217-v1.pl
+++ project-euler/217/euler-217-v1.pl
@@ -62,4 +62,34 @@ continue
     $pow10 *= 10;
 }
 
+my @unique_results;
+my @T;
+
+$unique_results[0] = 0;
+$unique_results[1] = 45;
+
+$T[0] = 0;
+$T[1] = $unique_results[1] + $T[0];
+
+for my $num_digits (2 .. 47)
+{
+    my $half = ($num_digits >> 1);
+    my $parity = ($num_digits & 0x1);
+    my $result = 0;
+    foreach my $sum (@{$S[$half]})
+    {
+        my $count_true = $sum->[1]->[0] // 0;
+        my $sum_true = $sum->[1]->[1] // 0;
+        my $count_both = $count_true + ($sum->[0]->[0] // 0);
+        my $sum_both = $sum_true + ($sum->[0]->[1] // 0);
+        $result += ($sum_true * $count_both * (10 ** ($half + $parity)) + $sum_both * $count_true) * ($parity ? 10 : 1) + ($parity ? (45 * $count_both * $count_true * (10 ** $half)) : 0);
+
+    }
+    $unique_results[$num_digits] = $result;
+    push @T, $T[-1] + $result;
+}
+
+print "T(2) = ", $T[2], "\n";
+print "T(5) = ", $T[5], "\n";
+print "T(47) mod 3**15 = ", ($T[47] % (3 ** 15)), "\n";
 
