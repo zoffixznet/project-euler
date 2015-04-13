a1da01e68dc24e9c579729b8e8e8062eb1381999
Shlomi Fish <shlomif@shlomifish.org>
2015-04-13 18:11:37 +0300

    Add the correct solution for #225.

---
+++ project-euler/225/euler-225-v1.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+# use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my $n = 3;
+
+my $found = 0;
+while ($found < 124)
+{
+    my @last = (1,1,1);
+    my %sigs = ("@last" => undef(),);
+
+    L:
+    while ($last[-1])
+    {
+        @last = (@last[1,2], ($last[0]+$last[1]+$last[2])%$n);
+        if (exists($sigs{"@last"}))
+        {
+            $found++;
+            print "Found the ${found}'th find - $n\n";
+            last L;
+        }
+        $sigs{"@last"} = undef();
+    }
+}
+continue
+{
+    $n += 2;
+}
