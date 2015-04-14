2b13000dd875c0ae7e69b63d4e7147b7b0ddbbac
Shlomi Fish <shlomif@shlomifish.org>
2015-04-14 13:28:00 +0300

    Wrong solution to Euler #268.

---
+++ project-euler/268/euler-268-v1.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+# use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(min max sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my @primes = map { 0 + $_ } `primes 2 100`;
+
+my $L = (10 ** 16);
+
+my $total = 0;
+
+my $MAX_C = (1 << 25);
+my $ITER = 100_000;
+my $count = 0;
+my $next_iter = $ITER;
+sub f
+{
+    # $i is index to start from.
+    # $c is count.
+    # $mul is the product
+    my ($i, $c, $mul) = @_;
+
+    if ($i == @primes)
+    {
+        if (++$count == $next_iter)
+        {
+            print "Reached $count/$MAX_C\n";
+            $next_iter += $ITER;
+        }
+        if ($c >= 4)
+        {
+            my $offset = int( $L / $mul );
+            if ($c & 0x1)
+            {
+                $total -= $offset;
+            }
+            else
+            {
+                $total += $offset;
+            }
+        }
+    }
+    else
+    {
+        f($i + 1, $c, $mul);
+        f($i + 1, $c+1, $mul*$primes[$i]);
+    }
+    return;
+}
+
+f(0, 0, 1);
+
+print "total = $total\n";
