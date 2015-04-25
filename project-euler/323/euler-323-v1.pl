dcdb5e1415a0a7cc4a66e7d0daca277f1e91066c
Shlomi Fish <shlomif@shlomifish.org>
2015-04-25 11:57:06 +0300

    Add successful solution of Euler #323.
    That was quite easy.

---
+++ project-euler/323/euler-323-v1.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $n = 1;
+my $expected_n = 0;
+my $exp = 0.5;
+my $prev_prob = 0;
+
+while (1)
+{
+    my $next_prob = (1 - $exp)**32;
+    $expected_n += $n * ($next_prob - $prev_prob);
+    $prev_prob = $next_prob;
+    printf "Exp[n] = %.10f\n", $expected_n;
+}
+continue
+{
+    $n++;
+    $exp *= 0.5;
+}
