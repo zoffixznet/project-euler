a0176469e125d44338d8653689f3543b59223400
Shlomi Fish <shlomif@shlomifish.org>
2015-04-30 16:44:49 +0300

    Add the incorrect solution for Euler #151.

---
+++ project-euler/151/euler-151-v1.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use List::Util qw(sum);
+use List::MoreUtils qw(all);
+
+STDOUT->autoflush(1);
+
+my $total_num = 0;
+my $sum = 0;
+
+# rec is short for "recurse".
+sub rec
+{
+    my ($factor, $counts, $result) = @_;
+
+    my $cnt = sum(@$counts);
+    if ($cnt == 0)
+    {
+        $sum += $factor * $result;
+        $total_num += $factor;
+    }
+    else
+    {
+        while (my ($size, $f) = each(@$counts))
+        {
+            if ($f > 0)
+            {
+                my @c = @$counts;
+                $c[$size]--;
+                for my $s ($size+1 .. 4)
+                {
+                    $c[$s]++;
+                }
+                rec ($factor*$f, (\@c), $result + ($cnt == 1));
+            }
+        }
+    }
+    return;
+}
+
+rec(1,[1],0);
+
+printf "Result == %.6f\n", ($sum / $total_num - 2);
