961406f5502fd299592490de788e6752535ac315
Shlomi Fish <shlomif@shlomifish.org>
2015-05-20 17:24:16 +0300

    Perl program now yields the correct solution. Yay!
    It's time consuming though.

--- project-euler/167/euler-167-v1.pl
+++ project-euler/167/euler-167-v1.pl
@@ -24,21 +24,30 @@ sub solve
     my $AA = 2;
     my $BB = $n*2+1;
 
-    my @u = ($AA, $BB);
+    my $U = 0;
+    my $u = '';
+    vec($u, $U++,32) = $AA;
+    vec($u, $U++,32) = $BB;
+
     print "$AA\n$BB\n";
 
     my $v = '';
     vec($v, $AA+$BB, 2) = 1;
 
-    my $CALC_CNT = $n * 2_000;
-    my $FIND_NUM = $n * 1000;
-    my $MAX_STEP = $n * 500;
+    my $CALC_CNT = (($n < 5) ? ($n * 2_000) : ($n < 8) ? ($n * 16_000) : ($n == 8) ? ($n * 500_000) : ($n == 9) ? ($n * 1_000_000) : ($n * 4_000_000));
+    my $FIND_NUM = ($CALC_CNT >> 2);
+    my $MAX_STEP = ($FIND_NUM >> 2);
 
-    while (@u < $CALC_CNT)
+    my $double_good_next;
+    my $even;
+
+    my $next;
+    while (not (defined($double_good_next) and $next > $double_good_next))
     {
-        my $next;
+        $next = undef;
+        my $LAST_u = vec($u, $U-1, 32);
         FIND_NEXT:
-        for my $i ($u[-1]+1 .. ($u[-1]<<1))
+        for my $i ($LAST_u+1 .. ($LAST_u<<1))
         {
             if (vec($v, $i, 2) == 1)
             {
@@ -52,35 +61,77 @@ sub solve
             die "Next not found!";
         }
 
-        for my $prev (@u)
+        for my $prev_i (0 .. $U-1)
         {
-            my $s = $prev+$next;
+            my $s = vec($u, $prev_i, 32) + $next;
             if (vec($v, $s, 2) < 2)
             {
                 vec($v, $s, 2)++;
             }
         }
 
-        print "$next\n";
-        push @u, $next;
+        # print "$next\n";
+        vec($u, $U++, 32) = $next;
+
+        if (not ($next & 0x1))
+        {
+            $double_good_next = (($even = $next) << 1);
+        }
     }
 
-    my $M = $#u;
+    while ($U < $CALC_CNT)
+    {
+        my $val = sub {
+            # Pivot for 2
+            my $p2 = $U-1;
+            # Pivot for even.
+            my $pe = $p2-1;
+
+            my $LAST_u = vec($u, $p2, 32);
+
+            my $val = $LAST_u+$AA;
+            my $MAX_VAL = $LAST_u+$even;
+
+            while (vec($u,$pe,32) + $even > $val)
+            {
+                $pe--;
+            }
+            if (vec($u,$pe,32)+$even != $val)
+            {
+                return $val;
+            }
+            while (($val += 2) < $MAX_VAL)
+            {
+                while (vec($u,$pe,32)+$even < $val)
+                {
+                    $pe++;
+                }
+                if (vec($u,$pe,32)+$even == $val)
+                {
+                    return $val;
+                }
+            }
+            return $val;
+        }->();
+        vec($u, $U++, 32) = $val;
+    }
+
+    my $M = $U-1;
     STEP:
     for my $STEP (1 .. $MAX_STEP)
     {
-        my $delta = $u[$M]-$u[$M-$STEP];
+        my $delta = vec($u,$M,32)-vec($u,$M-$STEP,32);
 
         for my $i ($M-$FIND_NUM .. $M-$STEP-1)
         {
-            if ($u[$i+$STEP] - $u[$i] != $delta)
+            if (vec($u,$i+$STEP,32) - vec($u,$i,32) != $delta)
             {
                 next STEP;
             }
         }
         my $STEP_K = $DESIRED_K;
         $STEP_K -= ((int (($STEP_K - $M) / $STEP) + 1) * $STEP);
-        return $u[$STEP_K] + $delta * (($DESIRED_K - $STEP_K) / $STEP);
+        return vec($u,$STEP_K,32) + $delta * (($DESIRED_K - $STEP_K) / $STEP);
     }
 
     die "Cannot find for $n!";
@@ -90,5 +141,6 @@ my $total_sum = 0;
 for my $n (2 .. 10)
 {
     $total_sum += solve($n);
+    print "Total Sum [n=$n] = $total_sum\n";
 }
 print "Total Sum = $total_sum\n";
