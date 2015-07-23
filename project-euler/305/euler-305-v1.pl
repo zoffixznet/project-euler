c3a00a23045b6ffcd02026821a13448d8979a6fd
Shlomi Fish <shlomif@shlomifish.org>
2015-07-23 19:16:14 +0300

    Add a non-working version of Euler #305.
    It needs more work.

---
+++ project-euler/305/euler-305-v1.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my $n = shift(@ARGV);
+
+my %mins;
+
+$mins{contained} =
+{
+    next => 1,
+    strs => [{ s => '', strs => {next => 1, str => '',}},],
+};
+
+my %mm = (map { $_ => +{ next => 1, s => '' } } 1 .. length($n)-1);
+sub start_10
+{
+    my $l = shift;
+    if ($l == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return +($l-1) * (9 * 10 ** ($l-1)) + start_10($l-1);
+    }
+}
+
+sub calc_start
+{
+    my $needle = shift;
+
+    my $len = length($needle);
+
+    return start_10($len) + ($needle - (10 ** ($len-1))) * $len;
+}
+
+my $last_pos;
+my $count = 1;
+while (1)
+{
+    my $min;
+    # For within a number containing the full number:
+
+    my $contained = $mins{contained}{strs};
+
+    my $last_i;
+    my $which = '';
+    while (my ($i, $rec) = each@$contained)
+    {
+        my $prefix = $rec->{'s'};
+        my $suffix = $rec->{strs}->{str};
+
+        my $needle = $prefix.$n.$suffix;
+
+        my $pos = calc_start($needle) + length($prefix);
+        if (!defined($min) || $pos < $min)
+        {
+            $last_i = $i;
+            $min = $pos;
+            $which = 'contained';
+        }
+    }
+    if ($last_i == $#$contained)
+    {
+        push @{ $mins{contained}{strs} }, +{ s => ($mins{contained}{'next'}++), strs => {next => 1, str => ''} };
+    }
+
+    my $last_s;
+    for my $start_new_pos (1 .. length($n)-1)
+    {
+        my $prefix = substr($n, $start_new_pos);
+        my $middle = $mm{$start_new_pos}{'s'};
+        my $suffix = substr($n, 0, $start_new_pos);
+
+        my $needle = $prefix.$middle.$suffix;
+
+        my $pos = calc_start($needle) + length($prefix)+length($middle);
+
+        if (!defined($min) || $pos < $min)
+        {
+            $last_s = $start_new_pos;
+            $which = 'mid';
+            $min = $pos;
+        }
+    }
+
+    my $rec;
+    if ($which eq 'mid')
+    {
+        $rec = $mm{$last_s};
+    }
+    else
+    {
+        $rec = $mins{contained}{strs}[$last_i];
+    }
+    $rec->{'s'} = $rec->{'next'}++;
+    print $count++, " $min\n";
+}
