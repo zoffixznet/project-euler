4748cb01d442dddbf4ffe14bcebfc033b34a9456
Shlomi Fish <shlomif@shlomifish.org>
2015-09-01 19:46:07 +0300

    Add the solution - type 'make Summary.txt' to generate

---
+++ project-euler/238/summarize.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+my $LIM = eval ("2" . '0'x15);
+my $S = 80846691;
+my $M = $LIM % $S;
+my $T = int($LIM / $S);
+my $U = $T + 1;
+
+# Result
+my $R = 0;
+
+open my $in, '<', 'sums.txt';
+while (my $l = <$in>)
+{
+    if (my ($s, $p) = $l =~ m#\Ap\[([0-9]+)\] = ([0-9]+)\n#)
+    {
+        $R += $p * ($s <= $M ? $U : $T);
+    }
+}
+close($in);
+
+print "Result = $R\n";
