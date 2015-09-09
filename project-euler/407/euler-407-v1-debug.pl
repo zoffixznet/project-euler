3ddcc8792c59483b0025e9c18e7cea838113e920
Shlomi Fish <shlomif@shlomifish.org>
2015-09-09 18:00:59 +0300

    Add the C++ version - still too slow

---
+++ project-euler/407/euler-407-v1-debug.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+# use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my $sum = 0;
+
+my $STEP = 1_000;
+my $print_at = $STEP;
+for my $n (1 .. 10_000_000)
+{
+    my $r = sub {
+        for (my $x=$n-1;$x>1;$x--)
+        {
+            if ((($x*$x) % $n) == $x)
+            {
+                return $x;
+            }
+        }
+        return 1;
+    }->();
+    print "f($n) = $r\n";
+    $sum += $n;
+    if ($n == $print_at)
+    {
+        print "N=$n Sum = $sum\n";
+        $print_at += $STEP;
+    }
+}
+
+print "Final sum = $sum\n";
---
+++ project-euler/407/euler_407_v1.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+typedef long long ll;
+
+const ll STEP = 1000;
+
+static inline const ll calc(const ll n)
+{
+    for (ll x=n-1;x>1;x--)
+    {
+        if (((x*x) % n) == x)
+        {
+            return x;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char * argv[])
+{
+    ll sum = 0;
+
+    ll print_at = STEP;
+
+    for (ll n=1;n<=10000000;n++)
+    {
+        sum += calc(n);
+        if (n == print_at)
+        {
+            printf ("N=%lld Sum = %lld\n", n, sum);
+            fflush(stdout);
+            print_at += STEP;
+        }
+    }
+
+    printf ("Final sum = %lld\n", sum);
+    return 0;
+}
