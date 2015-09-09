328979e60c470d6dd304c13a5ab7dcc11bb01e1b
Shlomi Fish <shlomif@shlomifish.org>
2015-09-09 22:59:09 +0300

    Fix some bugs

--- project-euler/408/euler-408-v1.pl
+++ project-euler/408/euler-408-v1.pl
@@ -46,33 +46,37 @@ for my $path_len (1 .. $LIM)
         $y--;
     }
     push @$next, $prev->[-1];
-    print "Prev = [@$prev] ; Next = [@$next]\n";
+    # print "Prev = [@$prev] ; Next = [@$next]\n";
     $prev = $next;
 }
 
 my $max_x = $LIM;
+my $max_y = $LIM;
+my $min_x = 1;
 for my $path_len ($LIM+1 .. (($LIM << 1)))
 {
     my $next = [];
-    my $y = $path_len - 1;
-    for my $x (1 .. $max_x)
+    my $y = $max_y;
+    my $i = 1;
+    for my $x ($min_x .. $max_x)
     {
-        print "($x,$y,$path_len)\n";
+        # print "($x,$y,@{[$x+$y,$path_len]})\n";
         if (vec($is_sq, $path_len, 1) && vec($is_sq, $x, 1) && vec($is_sq, $y, 1))
         {
             push @$next, 0;
         }
         else
         {
-            push @$next, (($prev->[$x-1]+$prev->[$x]) % $MOD);
+            push @$next, (($prev->[$i-1]+$prev->[$i]) % $MOD);
         }
     }
     continue
     {
+        $i++;
         $y--;
     }
-    $max_x--;
-    print "Prev = [@$prev] ; Next = [@$next]\n";
+    $min_x++;
+    # print "Prev = [@$prev] ; Next = [@$next]\n";
     $prev = $next;
 }
 
