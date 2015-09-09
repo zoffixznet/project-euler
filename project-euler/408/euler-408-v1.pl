6f9304b8e6277785da3b3aaa221198af9b3f5154
Shlomi Fish <shlomif@shlomifish.org>
2015-09-09 22:21:50 +0300

    Wrong solution to Euler #408

---
+++ project-euler/408/euler-408-v1.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my $is_sq = '';
+
+my $LIM = shift(@ARGV);
+my $MOD = 1_000_000_007;
+for my $x (0 .. sqrt(2*$LIM))
+{
+    vec($is_sq, $x*$x, 1) = 1;
+}
+
+# $prev maps ($x) => P($x, $path_len-$x)
+my $prev = [1];
+for my $path_len (1 .. $LIM)
+{
+    my $next = [];
+    for my $x (0)
+    {
+        push @$next, $prev->[0];
+    }
+    my $y = $path_len - 1;
+    for my $x (1 .. $path_len - 1)
+    {
+        if (vec($is_sq, $path_len, 1) && vec($is_sq, $x, 1) && vec($is_sq, ($y--), 1))
+        {
+            push @$next, 0;
+        }
+        else
+        {
+            push @$next, (($prev->[$x-1]+$prev->[$x]) % $MOD);
+        }
+    }
+    push @$next, $prev->[-1];
+    $prev = $next;
+}
+
+my $max_x = $LIM;
+for my $path_len ($LIM+1 .. (($LIM << 1) - 1))
+{
+    my $next = [];
+    my $y = $path_len - 1;
+    for my $x (1 .. $max_x)
+    {
+        if (vec($is_sq, $path_len, 1) && vec($is_sq, $x, 1) && vec($is_sq, ($y--), 1))
+        {
+            push @$next, 0;
+        }
+        else
+        {
+            push @$next, (($prev->[$x-1]+$prev->[$x]) % $MOD);
+        }
+    }
+    $max_x--;
+    print "Prev = [@$prev] ; Next = [@$next]\n";
+    $prev = $next;
+}
+
+print "Result = ", $prev->[0], "\n";
