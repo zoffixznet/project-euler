a6bbfa9258be7417c012b2a10ede2c372fbc52f7
Shlomi Fish <shlomif@shlomifish.org>
2015-09-09 17:14:44 +0300

    Add first version - too slow

---
+++ project-euler/407/euler-407-v1.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+# use Math::BigInt lib => 'GMP', ':constant';
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+my $sum = 0;
+
+my $STEP = 1_000;
+my $print_at = $STEP;
+for my $n (1 .. 10_000_000)
+{
+    $sum += sub {
+        for (my $x=$n-1;$x>1;$x--)
+        {
+            if ((($x*$x) % $n) == $x)
+            {
+                return $x;
+            }
+        }
+        return 1;
+    }->();
+    if ($n == $print_at)
+    {
+        print "N=$n Sum = $sum\n";
+        $print_at += $STEP;
+    }
+}
+
+print "Final sum = $sum\n";
