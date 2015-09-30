94ceb0793799f115d82b078d46f6b509826ad13d
Shlomi Fish <shlomif@shlomifish.org>
2015-09-30 18:46:59 +0300

    Start adding #282 - Ackermann

---
+++ project-euler/282/euler-282-v1.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+no warnings 'recursion';
+
+use Math::GMP;
+
+use integer;
+use bytes;
+
+use List::Util qw(sum);
+use List::MoreUtils qw();
+
+STDOUT->autoflush(1);
+
+# The cache.
+my %c;
+
+sub A
+{
+    my ($m, $n) = @_;
+
+    my $ret = sub {
+        if ($m == 0)
+        {
+            return Math::GMP->new("$n")+1;
+        }
+        elsif ($m == 1)
+        {
+            return Math::GMP->new("$n")+2;
+        }
+        elsif ($m == 2)
+        {
+            return ((Math::GMP->new("$n")<<1)+3);
+        }
+        elsif ($m == 3)
+        {
+            return ((Math::GMP->new('1') << (Math::GMP->new("$n") + 3)) - 3);
+        }
+        elsif ($n == 0)
+        {
+            return A($m-1,1);
+        }
+        else
+        {
+            return $c{"$m"}{"$n"} //= A($m-1, A($m,$n-1));
+        }
+    }->();
+
+    if ($ret <= 0)
+    {
+        die "A($m,$n)!";
+    }
+
+    # print "A($m,$n) = $ret\n";
+
+    return $ret;
+}
+
+my $MOD = 14 ** 8;
+
+sub A_m_is_4_mod
+{
+    my ($m, $n) = @_;
+
+    my $ret = sub {
+        if ($m == 0)
+        {
+            return Math::GMP->new("$n")+1;
+        }
+        elsif ($m == 1)
+        {
+            return Math::GMP->new("$n")+2;
+        }
+        elsif ($m == 2)
+        {
+            return ((Math::GMP->new("$n")<<1)+3);
+        }
+        elsif ($m == 3)
+        {
+            return ((Math::GMP->new('1') << (Math::GMP->new("$n") + 3)) - 3);
+        }
+        elsif ($n == 0)
+        {
+            return A($m-1,1);
+        }
+        elsif ($m == 4)
+        {
+            my $ret = A_m_is_4_mod(4, $n-1);
+            return ((Math::GMP->new('1') << ($ret + 3)) - 3) % $MOD;
+        }
+        else
+        {
+            return $c{"$m"}{"$n"} //= A($m-1, A($m,$n-1));
+        }
+    }->();
+
+    if ($ret <= 0)
+    {
+        die "A($m,$n)!";
+    }
+
+    # print "A($m,$n) = $ret\n";
+
+    return $ret;
+}
+
+for my $m (0 .. 4)
+{
+    for my $n (0 .. 100)
+    {
+        print "A($m,$n) = ", A($m,$n), "\n";
+    }
+}
+
+for my $x (0 .. 6)
+{
+    print "A($x,$x) = ", A($x,$x), "\n";
+}
