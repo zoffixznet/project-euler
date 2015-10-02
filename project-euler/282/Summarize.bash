3f5ad03bc080f1a36206180dd18d0e03cbdd1690
Shlomi Fish <shlomif@shlomifish.org>
2015-10-02 17:02:03 +0300

    Made it safer - still wrong solution

--- project-euler/282/Summarize.bash
+++ project-euler/282/Summarize.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-perl -lanE 'print if m#\AA(?:_mod)?\(([0-6]),\1\) = #' | perl -lanE '$s += $F[-1]; END { print $s % (14 ** 8) }'
+perl -lanE 'print if m#\AA(?:_mod)?\(([0-6]),\1\) = #' | perl -lanE '($s += $F[-1]) %= (14 ** 8); END { print $s; }'
