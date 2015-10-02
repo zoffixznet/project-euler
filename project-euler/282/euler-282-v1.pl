dbe226a1ddbc3738bd12dc06c49136d7ae09259f
Shlomi Fish <shlomif@shlomifish.org>
2015-10-02 21:16:20 +0300

    some bug fixes and validation code - still fails.

--- project-euler/282/euler-282-v1.pl
+++ project-euler/282/euler-282-v1.pl
@@ -112,6 +112,7 @@ sub A_mod
         {
             return ((($n<<1)+3)%$MOD);
         }
+        # elsif (0)
         elsif ($m == 3)
         {
             return ((((Math::GMP->new('1') << (Math::GMP->new("$n") + 3)) - 3)%$MOD) . '');
@@ -167,19 +168,82 @@ for my $m (0 .. 3)
     }
 }
 
+sub exp_mod
+{
+    my ($base, $exp, $mod) = @_;
+
+    if ($exp == 0)
+    {
+        return 1;
+    }
+    my $rec = exp_mod($base, $exp >> 1, $mod);
+    my $ret = ($rec * $rec) % $mod;
+    if ($exp & 1)
+    {
+        $ret = (($ret * $base) % $mod);
+    }
+    return $ret;
+}
+
+sub hyperexp_modulo
+{
+    my ($base, $exp, $mod, $prefix, $DEFAULT_RET) = @_;
+
+    if ($exp == 1)
+    {
+        return ($base % $mod);
+    }
+    if (($mod == 1) and ($prefix == 0))
+    {
+        return $DEFAULT_RET;
+    }
+
+    my $mod1 = $base;
+    my $e = 0;
+
+    my %cache = ($mod1 => $e);
+    ($mod1 *= $base) %= $mod;
+
+    while (!exists($cache{$mod1}))
+    {
+        $cache{$mod1} = $e++;
+        ($mod1 *= $base) %= $mod;
+    }
+
+    my $PREFIX = $cache{$mod1};
+    my $LEN = $e - $PREFIX;
+
+    my $mod_recurse = hyperexp_modulo($base, $exp-1, $LEN, $PREFIX, $base);
+
+    my $ret = 1;
+    for my $i (1 .. $mod_recurse)
+    {
+        ($ret *= $base) %= $mod;
+    }
+    return $ret;
+}
+
+my $TEST_MOD = 37;
 for my $m (4)
 {
     for my $n (0 .. 2)
     {
-        print "A($m,$n) = ", (A($m,$n) % $ULTIMATE_MOD), "\n";
+        # print "A($m,$n) = ", (A($m,$n) % $ULTIMATE_MOD), "\n";
+        print "A($m,$n) = ", (A($m,$n) % $TEST_MOD), "\n";
+        print "A[t]($m,$n) = ", ((hyperexp_modulo(2, $n+3, $TEST_MOD, 0, [0 .. $TEST_MOD-1]) + $TEST_MOD - 3) % $TEST_MOD), "\n";
+    }
+    for my $n (3 .. 10)
+    {
+        print "A($m,$n) = ", ((hyperexp_modulo(2, $n+3, $TEST_MOD, 0, [0 .. $TEST_MOD-1]) + $TEST_MOD - 3) % $TEST_MOD), "\n";
     }
 }
 
 for my $m (4)
 {
-    for my $n (0 .. 4)
+    for my $n (0 .. 10)
     {
-        print "A_mod($m,$n) = ", A_mod($m, $n, $ULTIMATE_MOD, 0), "\n";
+        # print "A_mod($m,$n) = ", A_mod($m, $n, $ULTIMATE_MOD, 0), "\n";
+        print "A_mod($m,$n) = ", A_mod($m, $n, $TEST_MOD, 0), "\n";
     }
 }
 
