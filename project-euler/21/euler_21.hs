93d91af1e835838ef105da8b00072fe08a6486e6
Shlomi Fish <shlomif@shlomifish.org>
2015-10-11 15:11:30 +0300

    Add the haskell version of Euler #21.

---
+++ project-euler/21/euler_21.hs
@@ -0,0 +1,12 @@
+import Data.Array
+
+max_ = 200000
+
+gen 100001 = []
+gen n = [(i*n,n)|i <- [2 .. max_ `div` n]] ++ (gen (n+1))
+
+arr = accumArray (+) 0 (0,max_) (gen 1)
+
+result = sum $ filter (\a -> let b = (arr!a) in b /= a && (arr!b) == a) [1 .. (10000 - 1)]
+
+main = putStrLn $ show result
