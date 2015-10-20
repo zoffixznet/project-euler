199e13d275443f0aa19e000df0ed0b18d4a1b167
Shlomi Fish <shlomif@shlomifish.org>
2015-10-20 15:26:28 +0300

    Refactoring: remove unnecessary code.

--- project-euler/294/euler-294-v1.pl
+++ project-euler/294/euler-294-v1.pl
@@ -35,6 +35,7 @@ sub exp_mod
 
     return ($ret % $MOD);
 }
+
 sub inc
 {
     my ($n) = @_;
@@ -43,26 +44,25 @@ sub inc
 
     my $new_n = $n + 1;
 
-    if (!exists($for_n{$new_n})) {
-        my $new_rec = +{};
+    my $new_rec = +{};
 
-        my $BASE_MOD = exp_mod(23, 10, $new_n);
+    my $BASE_MOD = exp_mod(23, 10, $new_n);
 
-        while (my ($old_count_digits, $old_mod_counts) = each (%{$old_rec}))
+    while (my ($old_count_digits, $old_mod_counts) = each (%{$old_rec}))
+    {
+        while (my ($old_mod, $old_count) = each(%$old_mod_counts))
         {
-            while (my ($old_mod, $old_count) = each(%$old_mod_counts))
+            for my $new_digit (0 .. min(9, 23-$old_count_digits))
             {
-                for my $new_digit (0 .. min(9, 23-$old_count_digits))
-                {
-                    (($new_rec->{$old_count_digits + $new_digit}
+                (($new_rec->{$old_count_digits + $new_digit}
                         ->{($old_mod + $BASE_MOD * $new_digit) % 23}
                         //= 0) += $old_count)
-                    %= 1_000_000_000;
-                }
+                %= 1_000_000_000;
             }
         }
-        $for_n{$new_n} = $new_rec;
     }
+
+    $for_n{$new_n} = $new_rec;
 
     return;
 }
@@ -76,48 +76,46 @@ sub double
 
     my $new_n = ($n << 1);
 
-    if (!exists($for_n{$new_n})) {
-        my $old_rec = $for_n{$n};
+    my $old_rec = $for_n{$n};
 
-        my $BASE_MOD = exp_mod(23, 10, $n);
+    my $BASE_MOD = exp_mod(23, 10, $n);
 
-        my $new_rec = +{};
+    my $new_rec = +{};
 
-        # cd == count_digits
-        foreach my $cd_low (0 .. $COUNT_DIGITS)
+    # cd == count_digits
+    foreach my $cd_low (0 .. $COUNT_DIGITS)
+    {
+        my $mod_counts_low = $old_rec->{$cd_low};
+
+        foreach my $mod_low (0 .. $HIGH_MOD)
         {
-            my $mod_counts_low = $old_rec->{$cd_low};
+            my $low_count = $mod_counts_low->{$mod_low} // 0;
 
-            foreach my $mod_low (0 .. $HIGH_MOD)
+            foreach my $cd_high (0 .. $COUNT_DIGITS-$cd_low)
             {
-                my $low_count = $mod_counts_low->{$mod_low} // 0;
+                my $mod_counts_high = $old_rec->{$cd_high};
 
-                foreach my $cd_high (0 .. $COUNT_DIGITS-$cd_low)
+                foreach my $proto_mod_high (0 .. $HIGH_MOD)
                 {
-                    my $mod_counts_high = $old_rec->{$cd_high};
+                    my $high_count = $mod_counts_high->{$proto_mod_high} // 0;
 
-                    foreach my $proto_mod_high (0 .. $HIGH_MOD)
-                    {
-                        my $high_count = $mod_counts_high->{$proto_mod_high} // 0;
-
-                        (($new_rec->{
-                            $cd_low + $cd_high
-                        }
-                        ->{
-                            ($mod_low + $BASE_MOD * $proto_mod_high)
+                    (($new_rec->{
+                                $cd_low + $cd_high
+                            }
+                            ->{
+                                ($mod_low + $BASE_MOD * $proto_mod_high)
                                 %
-                            23
-                        } //= 0) += ($high_count * $low_count))
-                        %= 1_000_000_000
-                        ;
+                                23
+                            } //= 0) += ($high_count * $low_count))
+                    %= 1_000_000_000
+                    ;
 
-                    }
                 }
             }
         }
-
-        $for_n{$new_n} = $new_rec;
     }
+
+    $for_n{$new_n} = $new_rec;
 
     return;
 }
