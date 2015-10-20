2be41baab7ec8d4fc7b60103c5d9e9aa47a99760
Shlomi Fish <shlomif@shlomifish.org>
2015-10-20 16:18:05 +0300

    correct count_digits to digits_sum.

--- project-euler/294/euler-294-v1.pl
+++ project-euler/294/euler-294-v1.pl
@@ -37,7 +37,7 @@ sub exp_mod
 }
 
 my $RESULT_MOD = 1_000_000_000;
-my $COUNT_DIGITS = 23;
+my $DIGITS_SUM = 23;
 my $BASE = 23;
 my $HIGH_MOD = $BASE - 1;
 
@@ -50,13 +50,13 @@ sub inc
 
     my $BASE_MOD = exp_mod($BASE, 10, $n+1);
 
-    while (my ($old_count_digits, $old_mod_counts) = each (@$old_rec))
+    while (my ($old_digits_sum, $old_mod_counts) = each (@$old_rec))
     {
-        for my $new_digit (0 .. min(9, $COUNT_DIGITS-$old_count_digits))
+        for my $new_digit (0 .. min(9, $DIGITS_SUM-$old_digits_sum))
         {
             while (my ($old_mod, $old_count) = each(@$old_mod_counts))
             {
-                (($new_rec->[$old_count_digits + $new_digit]
+                (($new_rec->[$old_digits_sum + $new_digit]
                         ->[($old_mod + $BASE_MOD * $new_digit) % $BASE]
                         //= 0) += $old_count)
                 %= $RESULT_MOD;
@@ -75,25 +75,25 @@ sub double
 
     my $new_rec = [];
 
-    # cd == count_digits
-    foreach my $low_cd (0 .. $COUNT_DIGITS)
+    # ds == digits_sum
+    foreach my $low_ds (0 .. $DIGITS_SUM)
     {
-        my $low_mod_counts = $old_rec->[$low_cd];
+        my $low_mod_counts = $old_rec->[$low_ds];
 
         foreach my $low_mod (0 .. $HIGH_MOD)
         {
             my $low_count = $low_mod_counts->[$low_mod] // 0;
 
-            foreach my $high_cd (0 .. $COUNT_DIGITS-$low_cd)
+            foreach my $high_ds (0 .. $DIGITS_SUM-$low_ds)
             {
-                my $high_mod_counts = $old_rec->[$high_cd];
+                my $high_mod_counts = $old_rec->[$high_ds];
 
                 foreach my $proto_high_mod (0 .. $HIGH_MOD)
                 {
                     my $high_count = $high_mod_counts->[$proto_high_mod] // 0;
 
                     (($new_rec->[
-                                $low_cd + $high_cd
+                                $low_ds + $high_ds
                             ]
                             ->[
                                 ($low_mod + $BASE_MOD * $proto_high_mod)
@@ -127,7 +127,7 @@ sub lookup
 {
     my ($n) = @_;
 
-    return (calc($n)->[$COUNT_DIGITS][0] // 0);
+    return (calc($n)->[$DIGITS_SUM][0] // 0);
 }
 
 print "Result[9] = @{[lookup(9)]}\n";
