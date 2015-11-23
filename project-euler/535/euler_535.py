c71b456570b65d370bfb91b21e30563e11587033
Shlomi Fish <shlomif@shlomifish.org>
2015-11-23 16:10:33 +0200

    Faster solution.
    Still could be much faster.

--- project-euler/535/euler_535.py
+++ project-euler/535/euler_535.py
@@ -14,23 +14,33 @@ class ArithSeq:
         self.i += 1
         return ret
 
+    def sum(self, cnt):
+        bottom = self.i
+        self.i += cnt
+        top = self.i - 1
+        return (((long(top) + long(bottom)) * long(cnt)) >> 1)
+
 class FracSeq:
     def __init__(self):
         self._a = ArithSeq()
         self._q = [['a',1,0],['f',1,1]]
         self._f = False
 
+    def _append(self):
+        if (not self._f):
+            self._f = FracSeq()
+            self._f.n()
+        new_peek = self._f.n()
+        self._q.append(['a', long(math.sqrt(new_peek)), 0])
+        self._q.append(['f', 1, new_peek])
+        return
+
     def n(self):
         first = self._q[0]
         (which, count, peek) = first[0], first[1], first[2]
         if (count == 0):
             if (which == 'f'):
-                if (not self._f):
-                    self._f = FracSeq()
-                    self._f.n()
-                new_peek = self._f.n()
-                self._q.append(['a', long(math.sqrt(new_peek)), 0])
-                self._q.append(['f', 1, new_peek])
+                self._append()
             self._q.pop(0)
             return self.n()
         first[1] -= 1
@@ -41,8 +51,29 @@ class FracSeq:
 
     def sum(self, cnt):
         ret = long(0)
-        for i in range(0,cnt):
-            ret += self.n()
+        # Trace
+        t = 1000000000
+        fc = 0
+        while (cnt > 0):
+            while (fc > t):
+                print ("Reached %d" % t)
+                t += 1000000000
+            first = self._q[0]
+            (which, count, peek) = first[0], first[1], first[2]
+            c = 0
+            if (which == 'a'):
+                c = (count if (count < cnt) else cnt)
+                ret += self._a.sum(c)
+            else:
+                c = 1
+                ret += peek
+                self._append()
+            cnt -= c
+            fc += c
+            if c == count:
+                self._q.pop(0)
+            else:
+                first[1] -= c
         return ret
 
 
@@ -55,3 +86,4 @@ def debug_sum(n):
 debug_sum(20)
 debug_sum(1000)
 debug_sum(1000000000)
+debug_sum(long('1000000000000000000'))
