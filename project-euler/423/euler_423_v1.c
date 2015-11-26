ea172eb6af07dec2f9dd04bb674e4c7dbc1ef7e1
Shlomi Fish <shlomif@shlomifish.org>
2015-11-26 12:22:00 +0200

    New version with some improvements and optimisations.

--- project-euler/423/euler_423_v1.c
+++ project-euler/423/euler_423_v1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+#define min(a,b) (((a)<(b))?(a):(b))
+
 typedef long long LL;
 const LL MOD = 1000000007;
 
-int primes[3001135];
+#define NUM_PRIMES_MIN_1 3001134
+#define NUM_PRIMES (NUM_PRIMES_MIN_1 + 1)
+
+int primes[NUM_PRIMES];
 int * p_p;
 
 LL s_n = 0;
@@ -11,12 +16,17 @@ LL s_n = 0;
 #define lim 50000000
 #define after_lim (lim+1)
 
-LL one[after_lim], two[after_lim];
+LL one[NUM_PRIMES], two[NUM_PRIMES];
 
 LL * current , * next, * temp;
 
+const int DUMP_STEP = 100000;
+
+int dump_at;
+
 int main()
 {
+    dump_at = DUMP_STEP;
     FILE * f = fopen ("primes.txt", "rt");
 
     p_p = primes;
@@ -35,14 +45,24 @@ int main()
     next = two;
     int pi = 0;
 
-    for (int n=1;n<after_lim; n++)
+    for (int n=1 ; n < after_lim ; n++)
     {
+        if (dump_at == n)
+        {
+            char fn[300];
+            sprintf(fn, "DUMPS/%d.bin", dump_at);
+            FILE * fh = fopen(fn, "wb");
+            fwrite (current, sizeof(*current), sizeof(one)/sizeof(one[0]), fh);
+            fclose(fh);
+            dump_at += DUMP_STEP;
+        }
         next[0] = (5 * current[0]) % MOD;
-        for (int i = 1; i <= n; i++)
+        const int my_top_idx = min(n, NUM_PRIMES_MIN_1);
+        for (int i = 1; i <= my_top_idx; i++)
         {
             next[i] = ((current[i]*5+current[i-1]) % MOD);
         }
-        next[n+1] = 0;
+        next[my_top_idx+1] = 0;
 
         if (*(p_p) == n)
         {
