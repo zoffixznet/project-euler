dc87b8cab49e4069cb59fdc722fc8479ccb1ac6e
Shlomi Fish <shlomif@shlomifish.org>
2015-12-20 18:19:05 +0200

    Memory consumption optimisation.

--- project-euler/539/euler_539_v1.py
+++ project-euler/539/euler_539_v1.py
@@ -1,25 +1,21 @@
 cache = {(1,False):0,(1,True):0}
 
-def P_list(L,is_left):
-    l = len(L)
+def P_list(l, is_left):
     key = (l,is_left)
     if not key in cache:
-        rec_L = range(0, l)
-        if is_left:
-            rec_L = filter(lambda x:(x & 1 == 1), rec_L)
-        else:
-            last = l - 1
-            rec_L = filter(lambda x:(x & 1 != last & 1), rec_L)
-        cache[key] = P_list(rec_L, (not is_left))
-    pos = cache[key]
-    return L[pos]
+        rec_l = (l >> 1)
+        # rec_start = start + (step if (is_left or ((l & 0x1) != 0)) else 0)
+        rec_start = 0 + (1 if (is_left or ((l & 0x1) != 0)) else 0)
+        rec_step = 2
+        cache[key] = rec_start + rec_step * P_list(rec_l, (not is_left))
+    return cache[key]
 
 def P(n):
     if n == 1:
         return 1
     if ((n & 0x1) == 1):
         return P(n-1)
-    return P_list((range(1,i+1)), True)
+    return 1 + P_list(i, True)
 
 for i in range(1,1001):
     print (("P(%d) = %d") % (i, P(i)))
