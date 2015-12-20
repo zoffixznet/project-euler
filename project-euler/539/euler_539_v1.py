81e46b3590c9c344bb49c8c8a78d994407d45c91
Shlomi Fish <shlomif@shlomifish.org>
2015-12-20 17:44:46 +0200

    Minor optimisations.

--- project-euler/539/euler_539_v1.py
+++ project-euler/539/euler_539_v1.py
@@ -1,6 +1,6 @@
 cache = {(1,False):0,(1,True):0}
 
-def P(L,is_left):
+def P_list(L,is_left):
     l = len(L)
     key = (l,is_left)
     if not key in cache:
@@ -10,9 +10,16 @@ def P(L,is_left):
         else:
             last = l - 1
             rec_L = filter(lambda x:(x & 1 != last & 1), rec_L)
-        cache[key] = P(rec_L, (not is_left))
+        cache[key] = P_list(rec_L, (not is_left))
     pos = cache[key]
     return L[pos]
 
+def P(n):
+    if n == 1:
+        return 1
+    if ((n & 0x1) == 1):
+        return P(n-1)
+    return P_list((range(1,i+1)), True)
+
 for i in range(1,1001):
-    print (("P(%d) = %d") % (i, P((range(1,i+1)), True)))
+    print (("P(%d) = %d") % (i, P(i)))
