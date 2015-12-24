4443e83226e8d2cf3e1feea6318df6ee83103ecc
Shlomi Fish <shlomif@shlomifish.org>
2015-12-24 18:06:19 +0200

    Optimise and clarify.

--- project-euler/539/euler_539_v1.py
+++ project-euler/539/euler_539_v1.py
@@ -10,22 +10,37 @@ def P_list(l, is_left):
                 )
     )
 
+def P_list(l, is_left):
+    if l == 1:
+        return 0
+    return ((l & 0x1) | is_left | \
+                (
+                    (
+                        P_list( (l >> 1), (is_left^0x1) )
+                    )
+                    << 1
+                )
+    )
+
+def P_l(l):
+    return P_list(l, 1)
+
 def P(n):
     if n == 1:
         return 1
     if ((n & 0x1) == 1):
         return P(n-1)
-    return 1 + P_list(i, True)
+    return 1 + P_l(i)
 
 def S(MAX):
     s = long(0)
     for i in xrange(2,MAX,2):
-        s += P_list(i, True)
+        s += P_l(i)
 
     s <<= 1
 
     if ((MAX & 0x1) == 0):
-        s += P_list(MAX, True)
+        s += P_l(MAX)
 
     s += MAX
 
