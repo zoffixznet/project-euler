43497929f8b2237fb9fe1d12986d1b526428fca3
Shlomi Fish <shlomif@shlomifish.org>
2015-12-24 17:20:40 +0200

    extract S and optimise it.
    Still too slow.

--- project-euler/539/euler_539_v1.py
+++ project-euler/539/euler_539_v1.py
@@ -17,7 +17,21 @@ def P(n):
         return P(n-1)
     return 1 + P_list(i, True)
 
-s = 0
-for i in xrange(1,1000000001):
-    s += P(i)
-print s
+def S(MAX):
+    s = long(0)
+    for i in xrange(2,MAX,2):
+        s += P_list(i, True)
+
+    s <<= 1
+
+    if ((MAX & 0x1) == 0):
+        s += P_list(MAX, True)
+
+    s += MAX
+
+    return s
+
+def print_S(MAX):
+    print (("S(%d) = %d") % (MAX, S(MAX)))
+
+print_S(1000000000)
