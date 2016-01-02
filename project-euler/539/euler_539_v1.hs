d5faee89c81d61729b2dd2e86002428e852f0e1b
Shlomi Fish <shlomif@shlomifish.org>
2016-01-02 17:42:53 +0200

    Fix a bug

--- project-euler/539/euler_539_v1.hs
+++ project-euler/539/euler_539_v1.hs
@@ -5,4 +5,4 @@ import Data.List
 p_l :: (Integer -> Int -> Integer)
 p_l 1 is_left = 0
 p_l l is_left = ((l .&. 0x1) .|. (toInteger is_left) .|. (sub `shiftL` 1)) where
-        sub = p_l (l `shiftR` 1) (is_left^0x1)
+        sub = p_l (l `shiftR` 1) (is_left `xor` 0x1)
