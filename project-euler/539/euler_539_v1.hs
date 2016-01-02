4699dbc7c3f8df475c078cb180d4f21212fb0e38
Shlomi Fish <shlomif@shlomifish.org>
2016-01-02 17:28:52 +0200

    Got to compile.

--- project-euler/539/euler_539_v1.hs
+++ project-euler/539/euler_539_v1.hs
@@ -4,5 +4,5 @@ import Data.List
 
 p_l :: (Integer -> Int -> Integer)
 p_l 1 is_left = 0
-p_l l is_left = ((l `and` 0x1) `or` is_left `or` (sub `shiftL` 1)) where
-        sub = p_l (l >> 1) (is_left^0x1)
+p_l l is_left = ((l .&. 0x1) .|. (toInteger is_left) .|. (sub `shiftL` 1)) where
+        sub = p_l (l `shiftR` 1) (is_left^0x1)
