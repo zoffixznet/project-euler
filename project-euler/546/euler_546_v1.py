46933ded3c3d6125132a03f4fb6104a2f2282400
Shlomi Fish <shlomif@shlomifish.org>
2016-02-28 14:13:19 +0200

    Convert to calculate the sum.

--- project-euler/546/euler_546_v1.py
+++ project-euler/546/euler_546_v1.py
@@ -7,24 +7,42 @@ if sys.version_info > (3,):
 
 
 cache = {}
+sum_cache = {}
 k = 2
 
 def _reset(new_k):
     """reset the cache."""
-    global cache
-    global k
+    global cache, k, sum_cache
     cache = {0:1}
+    sum_cache = {(-1):0, 0:1}
     k = new_k
     return
 
 _reset(2)
 
+# s(n) = sum_k=0^k=n(f(k)) = f(0)+f(1)+f(2)...+f(n)
+# f(n) = s(n)-s(n-1)
+# s(n) = f(kn) / k
+# f(n) = [ f(k(n+1)) - f(kn) ] / k
+# f(kn) = k * s(n)
+# s(kn)-s(kn-1) = k * s(n)
 def f(n):
     global cache, k
     if not n in cache:
         # cache[n] = long(sum([f(i/k) for i in xrange(0,n+1)]))
         cache[n] = long(f(n-1)+f(int(n/k)))
     return cache[n]
+
+def my_s(n):
+    global sum_cache, k
+    if not n in sum_cache:
+        # cache[n] = long(sum([f(i/k) for i in xrange(0,n+1)]))
+        x = int(n/k)
+        sum_cache[n] = long(((my_s(n-1))<<1)-my_s(n-2) + my_s(x)-my_s(x-1))
+    return sum_cache[n]
+
+def f(n):
+    return my_s(n)-my_s(n-1)
 
 def calc(new_k, n):
     _reset(new_k)
