16d725375d05349bfd69052710342c10047be6d9
Shlomi Fish <shlomif@shlomifish.org>
2016-03-20 21:36:56 +0200

    Add a hopefully more optimised version

---
+++ project-euler/552/euler-552-v3.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use integer;
+use bytes;
+
+# use Math::GMP;
+
+use Math::BigInt lib => 'GMP'; #, ':constant';
+
+use List::Util qw(reduce sum);
+use List::MoreUtils qw(all);
+
+use Math::ModInt qw(mod);
+use Math::ModInt::ChineseRemainder qw(cr_combine cr_extract);
+
+my $MAX = 300000;
+# p is the primes
+my @p = `primes 2 $MAX`;
+chomp(@p);
+
+STDOUT->autoflush(1);
+
+my $sum = 0;
+my $m = mod(1, 2);
+
+# s = search through.
+my $s = [@p];
+shift@p;
+while (my ($i, $P) = each@p)
+{
+    print "Reached i=$i\n";
+    $m = cr_combine($m, mod($i+2, Math::BigInt->new($P)));
+    my $A = $m->residue;
+
+    my @n;
+    while ($s->[0] <= $P)
+    {
+        shift@$s;
+    }
+    foreach my $p (@$s)
+    {
+        if ($A % $p == 0)
+        {
+            $sum += $p;
+            print "Found p=$p sum=$sum\n";
+        }
+        else
+        {
+            push @n, $p;
+        }
+    }
+    $s = \@n;
+}
+
+print "Sum == $sum\n";
