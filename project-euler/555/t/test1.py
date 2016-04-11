438e9d827d0fd6b354cc9b72f52f3e2b908e14b8
Shlomi Fish <shlomif@shlomifish.org>
2016-04-11 12:52:13 +0300

    Add another test

--- project-euler/555/t/test1.py
+++ project-euler/555/t/test1.py
@@ -9,13 +9,16 @@ from TAP.Simple import *
 
 from euler555 import M_func
 
-plan(1)
+plan(2)
 
 def main():
     M_91 = M_func(100,11,10)
 
     # TEST
     is_ok(M_91.calc(101), 91, "M[91](101) == 91")
+
+    # TEST
+    is_ok(M_91.calc(91), 91, "M[91](91) == 91")
 
 #----------------------------------------------------------------------
 
