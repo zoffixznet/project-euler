7a8e2242fd18d54939224fbb46aaf18e9dccf738
Shlomi Fish <shlomif@shlomifish.org>
2016-04-12 19:22:24 +0300

    Add the solution in perl

---
+++ project-euler/555/euler555.pl
@@ -0,0 +1,115 @@
+#!/usr/bin/perl
+
+# This program is based on euler-179.pl and on a solution of Euler no. 555
+# from the forums by "Mohammad_Ali_B". It is quite fast but consumes a lot
+# of RAM due to the caching.
+use strict;
+use warnings;
+use integer;
+
+use List::Util qw(reduce);
+
+my @Cache = (undef, []);
+
+sub factorize_helper
+{
+    my ($n, $start_from) = @_;
+
+    my $limit = int(sqrt($n));
+
+    if (! defined($Cache[$n]))
+    {
+        my $d = $n;
+        while ($d % $start_from)
+        {
+            if (++$start_from > $limit)
+            {
+                return $Cache[$n] = [[$n,1]];
+            }
+        }
+
+        $d /= $start_from;
+
+        my @n_factors = (map { [@$_] } @{factorize_helper($d, $start_from)});
+
+        if (@n_factors && $n_factors[0][0] == $start_from)
+        {
+            $n_factors[0][1]++;
+        }
+        else
+        {
+            unshift @n_factors, ([$start_from, 1]);
+        }
+
+        $Cache[$n] = \@n_factors;
+    }
+    return $Cache[$n];
+}
+
+sub factorize
+{
+    my ($n) = @_;
+    return factorize_helper($n, 2);
+}
+
+sub divisors
+{
+    my @factors = @{factorize(shift)};
+    my $rec;
+
+    $rec = sub {
+        my $l = shift;
+        if ($l == @factors)
+        {
+            return 1;
+        }
+        else
+        {
+            my @d = (1);
+            for my $i (1 .. $factors[$l][1])
+            {
+                push @d, $d[-1]*$factors[$l][0];
+            }
+            return map { my $x = $_; map { $_ * $x } @d } $rec->($l+1);
+        }
+    };
+
+    return $rec->(0);
+}
+
+sub faulhaber1
+{
+    my ($n) = @_;
+
+    return (($n * ($n+1)) >> 1);
+}
+
+sub sum
+{
+    my ($p, $m) = @_;
+    my $total = 0;
+
+    foreach my $s (1 .. $p-1)
+    {
+        foreach my $d (divisors($s))
+        {
+            my $k = $s + $d;
+            if ($k <= $p)
+            {
+                $total += faulhaber1($m+$k - ($s << 1)) - faulhaber1($m - $s);
+            }
+        }
+    }
+
+    return $total;
+}
+
+sub print_sum
+{
+    my ($p, $m) = @_;
+
+    printf "Sum(p=%d,m=%d) = %d\n", $p, $m, sum($p, $m);
+}
+
+print_sum(1000,1000);
+print_sum(1000000,1000000);
