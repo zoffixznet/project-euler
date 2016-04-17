37b95058f388e533a1a7d0d5833dd6e62eed8ebd
Shlomi Fish <shlomif@shlomifish.org>
2016-04-18 00:03:51 +0300

    Separate descriptions for copyright

--- project-euler/123/euler-123.pl
+++ project-euler/123/euler-123.pl
@@ -3,17 +3,6 @@
 use strict;
 use warnings;
 
-=head1 DESCRIPTION
-
-Let p_(n) be the nth prime: 2, 3, 5, 7, 11, ..., and let r be the remainder
-when (p_(n)−1)^(n) + (p_(n)+1)^(n) is divided by p_(n)^(2).
-
-For example, when n = 3, p_(3) = 5, and 4^(3) + 6^(3) = 280 ≡ 5 mod 25.
-
-The least value of n for which the remainder first exceeds 10^(9) is 7037.
-
-Find the least value of n for which the remainder first exceeds 10^(10).
-
 =head1 PLANNING
 
 (a-1)^(n) = (a+(-1))^n = Sigma_{k=0}^{n} { nCr(n,k) * a^k * (-1)^(n-k) }
--- project-euler/125/euler-125.pl
+++ project-euler/125/euler-125.pl
@@ -5,13 +5,6 @@ use warnings;
 
 use Math::BigInt lib => "GMP", ":constant";
 
-=head1 DESCRIPTION
-
-The palindromic number 595 is interesting because it can be written as the sum
-of consecutive squares: 6^2 + 7^2 + 8^2 + 9^2 + 10^2 + 11^2 + 12^2.
-
-There are exactly eleven palindromes below one-thousand that can be written as
-consecutive square sums, and the sum of these palindromes is 4164. Note that 1
 = 02 + 12 has not been included as this problem is concerned with the squares
 of positive integers.
 
