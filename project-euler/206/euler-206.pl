938732a809d96db5b2b246f62f7bdd1127cdcaae
Shlomi Fish <shlomif@shlomifish.org>
2016-04-17 23:32:02 +0300

    Separate description.

--- project-euler/206/euler-206.pl
+++ project-euler/206/euler-206.pl
@@ -3,16 +3,7 @@
 use strict;
 use warnings;
 
-=head1 DESCRIPTION
 
-Concealed Square
-
-Problem 206
-
-Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
-where each “_” is a single digit.
-
-=cut
 
 my $pat = '1_2_3_4_5_6_7_8_9_0';
 
