d52ba6a87ba09532f58ffae6ce9b5e974b6c4425
Shlomi Fish <shlomif@shlomifish.org>
2016-04-17 23:44:04 +0300

    Separate descriptions for copyright

--- project-euler/117/euler-117.pl
+++ project-euler/117/euler-117.pl
@@ -3,15 +3,7 @@
 use strict;
 use warnings;
 
-=head1 DESCRIPTION
 
-Using a combination of black square tiles and oblong tiles chosen from: red tiles measuring two units, green tiles measuring three units, and blue tiles measuring four units, it is possible to tile a row measuring five units in length in exactly fifteen different ways.
-
-How many ways can a row measuring fifty units in length be tiled?
-
-NOTE: This is related to problem 116.
-
-=cut
 
 use Math::GMP qw(:constant);
 
