2c0c1a8db2b2711ad20e82cc71c95bd61109187b
Shlomi Fish <shlomif@shlomifish.org>
2016-04-18 00:34:37 +0300

    Separate descriptions for copyright

--- project-euler/80/Euler80.pm
+++ project-euler/80/Euler80.pm
@@ -3,17 +3,6 @@ package Euler80;
 use strict;
 use warnings;
 
-=head1 Project Euler 80 Problem
-
-It is well known that if the square root of a natural number is not an integer, then it is irrational. The decimal expansion of such square roots is infinite without any repeating pattern at all.
-
-The square root of two is 1.41421356237309504880..., and the digital sum of the first one hundred decimal digits is 475.
-
-For the first one hundred natural numbers, find the total of the digital sums
-of the first one hundred decimal digits for all the irrational square roots.
-
-=cut
-
 use Math::BigInt (":constant", lib => 'GMP');
 
 my $required_digits = 100;
