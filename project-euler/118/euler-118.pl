0679fc62715bcb0bb772de40ac93d00579aff3ba
Shlomi Fish <shlomif@shlomifish.org>
2016-04-17 23:51:16 +0300

    Separate descriptions.
    Due to copyright considerations so it won't taint the code.

--- project-euler/118/euler-118.pl
+++ project-euler/118/euler-118.pl
@@ -8,16 +8,7 @@ use bytes;
 use List::MoreUtils qw(all);
 use List::Util qw(sum);
 
-=head1 DESCRIPTION
 
-Using all of the digits 1 through 9 and concatenating them freely to form
-decimal integers, different sets can be formed. Interestingly with the set
-{2,5,47,89,631}, all of the elements belonging to it are prime.
-
-How many distinct sets containing each of the digits one through nine exactly
-once contain only prime elements?
-
-=cut
 
 sub is_prime
 {
