a7e2cacac004a58232ec3eb8b13ede48ea03d004
Shlomi Fish <shlomif@shlomifish.org>
2016-04-18 00:08:23 +0300

    Separate descriptions for copyright

--- project-euler/108/euler-108.pl
+++ project-euler/108/euler-108.pl
@@ -3,22 +3,6 @@
 use strict;
 use warnings;
 
-=head1 DESCRIPTION:
-
-In the following equation x, y, and n are positive integers:
-
-1/x + 1/y = 1/n
-
-For n = 4 there are exactly three distinct solutions:
-
-* 1/5 + 1/20 = 1/4
-
-* 1/6 + 1/12 = 1/4
-
-* 1/8 + 1/8 = 1/4
-
-What is the least value of n for which the number of distinct solutions exceeds one-thousand?
-
 =head1 ANALYSIS
 
 ny + nx = xy
