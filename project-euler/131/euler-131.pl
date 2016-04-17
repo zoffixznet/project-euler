77d2624fac20427039e8f6e218836171943b41d9
Shlomi Fish <shlomif@shlomifish.org>
2016-04-17 23:14:59 +0300

    Separate descriptions for copyright

--- project-euler/131/euler-131.pl
+++ project-euler/131/euler-131.pl
@@ -8,18 +8,6 @@ use IO::Handle;
 STDOUT->autoflush(1);
 use Math::BigInt lib => 'GMP', ':constant';
 
-=head1 DESCRIPTION
-
-There are some prime values, p, for which there exists a positive integer, n,
-such that the expression n^3 + n^2p is a perfect cube.
-
-For example, when p = 19, 8^3 + 8^2×19 = 123.
-
-What is perhaps most surprising is that for each prime with this property the
-value of n is unique, and there are only four such primes below one-hundred.
-
-How many primes below one million have this remarkable property?
-
 =head1 ANALYSIS
 
 n^3+n^2*p = n^2(n+p).
