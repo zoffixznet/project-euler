0a276ee9ff45ff6b40345c3f14ed88a859ef1de4
Shlomi Fish <shlomif@shlomifish.org>
2016-04-18 15:41:12 +0300

    Add descriptions

--- project-euler/193/euler-193-v2-bruteforce-verify.bash
+++ project-euler/193/euler-193-v2-bruteforce-verify.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-seq 1 "$((MAX * MAX))" | xargs factor | perl -lane '$c++ if s/\A[^:]*://r !~ /( [0-9]+)\1(?: |\z)/; END { print $c; }'
+seq 1 "$((MAX * MAX))" | factor | perl -lane '$c++ if s/\A[^:]*://r !~ /( [0-9]+)\1(?: |\z)/; END { print $c; }'
