997b9d402d293ea05299335f62cb2b5f7c97c194
Shlomi Fish <shlomif@shlomifish.org>
2016-04-23 20:57:44 +0300

    Add a test

--- project-euler/423/t/pascal_sum.py
+++ project-euler/423/t/pascal_sum.py
@@ -8,7 +8,7 @@ def check_sum(n, p, val):
     if not ok (ret == val, "pascal_sum(%d,%d)" % (n,p)):
         diag("got = %d ; expected = %d" % (ret, val))
 
-plan(5)
+plan(6)
 
 # TEST
 check_sum(2,0,30)
@@ -24,3 +24,7 @@ check_sum(3,1,6*5*5+6*1*5+6*5*1)
 
 # TEST
 check_sum(3,2,6*6*6)
+
+# TEST
+check_sum(4,0,6*5*5*5)
+
