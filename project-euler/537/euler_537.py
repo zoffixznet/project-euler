696dbd6c22a5b804abf90d8866d19c473695ded0
Shlomi Fish <shlomif@shlomifish.org>
2016-04-28 20:12:31 +0300

    flake8 compliance.

--- project-euler/537/euler_537.py
+++ project-euler/537/euler_537.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import math
 import sys
 from subprocess import Popen, PIPE
 
@@ -10,13 +9,13 @@ if sys.version_info > (3,):
 BASE = 1004535809
 
 # Tuple is (n,k) where n is the sum and k is the count.
-cache = {(0,0):1,(1,1):1,(1,0):0,(0,1):1}
+cache = {(0, 0): 1, (1, 1): 1, (1, 0): 0, (0, 1): 1}
 
 pipe = Popen("primes 2", shell=True, stdout=PIPE).stdout
 
 last = 1
 counts = []
-for i in range(0,20001):
+for i in range(0, 20001):
     n = int(pipe.readline())
     counts.append(n-last)
     last = n
@@ -24,35 +23,37 @@ for i in range(0,20001):
 pipe.close()
 
 MAX = 20000
-# p = prev.
-def next_row(k,p):
+
+
+def next_row(k, p):
     if k == 0:
         return counts
     return [
-            sum([counts[i] * p[n-i] for i in range(0,n+1)]) % BASE
+            sum([counts[i] * p[n-i] for i in range(0, n+1)]) % BASE
             for n in range(0, MAX+1)
             ]
 
-def p_calc(n,k,r):
-    print ("T(%d,%d) = %d" % (n,k,r[n]))
+
+def p_calc(n, k, r):
+    print("T(%d,%d) = %d" % (n, k, r[n]))
 
 r = []
 k = 0
 while k < 3:
-    r = next_row(k,r)
+    r = next_row(k, r)
     k += 1
 
-p_calc(3,3,r)
+p_calc(3, 3, r)
 
 while k < 10:
-    r = next_row(k,r)
+    r = next_row(k, r)
     k += 1
 
-p_calc(10,10,r)
+p_calc(10, 10, r)
 
 while k < MAX:
-    r = next_row(k,r)
+    r = next_row(k, r)
     k += 1
-    print ("Reached k=%d" % (k))
+    print("Reached k=%d" % (k))
 
-p_calc(MAX,MAX,r)
+p_calc(MAX, MAX, r)
