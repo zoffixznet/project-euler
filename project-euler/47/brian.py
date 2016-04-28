067fa7cf6deef9339bf0f39c973f407257bac23e
Shlomi Fish <shlomif@shlomifish.org>
2016-04-28 20:21:57 +0300

    flake8

--- project-euler/47/brian.py
+++ project-euler/47/brian.py
@@ -1,18 +1,26 @@
-Limit=1000000     # Search under 1 million for now
-factors=[0]*Limit # number of prime factors.
-count=0
-for i in xrange(2,Limit):
-    if factors[i]==0:
+#!/usr/bin/env python
+
+import sys
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+Limit = 1000000         # Search under 1 million for now
+factors = [0] * Limit   # number of prime factors.
+count = 0
+for i in xrange(2, Limit):
+    if factors[i] == 0:
         # i is prime
-        count =0
-        val =i
+        count = 0
+        val = i
         while val < Limit:
             factors[val] += 1
-            val+=i
+            val += i
     elif factors[i] == 4:
-        count +=1
+        count += 1
         if count == 4:
-            print i-3 # First number
+            print(i-3)  # First number
             break
     else:
         count = 0
