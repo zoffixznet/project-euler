d879c8142c42f8434b0eaaa0f4e09814da079f2e
Shlomi Fish <shlomif@shlomifish.org>
2016-05-01 21:30:55 +0300

    flake8

--- project-euler/423/t/pascal_sum.py
+++ project-euler/423/t/pascal_sum.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
-from TAP.Simple import *
+from TAP.Simple import diag, ok, plan
 
 from euler_423_v2 import pascal_sum
 
+
 def check_sum(n, p, val):
     ret = pascal_sum(n, p)
-    if not ok (ret == val, "pascal_sum(%d,%d)" % (n,p)):
+    if not ok(ret == val, "pascal_sum(%d,%d)" % (n, p)):
         diag("got = %d ; expected = %d" % (ret, val))
 
 plan(7)
 
 # TEST
-check_sum(2,0,30)
+check_sum(2, 0, 30)
 
 # TEST
-check_sum(2,1,6*5+6*1)
+check_sum(2, 1, 6*5+6*1)
 
 # TEST
-check_sum(3,0,6*5*5)
+check_sum(3, 0, 6*5*5)
 
 # TEST
-check_sum(3,1,6*5*5+6*1*5+6*5*1)
+check_sum(3, 1, 6*5*5+6*1*5+6*5*1)
 
 # TEST
-check_sum(3,2,6*6*6)
+check_sum(3, 2, 6*6*6)
 
 # TEST
-check_sum(4,0,6*5*5*5)
+check_sum(4, 0, 6*5*5*5)
 
 # TEST
-check_sum(4,1,6*5*5*5+6*1*5*5+6*5*1*5+6*5*5*1)
-
+check_sum(4, 1, 6*5*5*5+6*1*5*5+6*5*1*5+6*5*5*1)
