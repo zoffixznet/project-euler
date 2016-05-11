0632b38048925d8a9ff817a43377b52f8496f0bb
Shlomi Fish <shlomif@shlomifish.org>
2016-05-11 15:05:27 +0300

    flake8

--- project-euler/548/euler_548_v1.py
+++ project-euler/548/euler_548_v1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import math
 import re
 import sys
 from subprocess import Popen, PIPE
@@ -9,7 +8,8 @@ if sys.version_info > (3,):
     long = int
     xrange = range
 
-cache = {'':long(1)}
+cache = {'': long(1)}
+
 
 def factor_sig(n):
     pipe = Popen(['factor', str(n)], shell=False, stdout=PIPE).stdout
@@ -18,20 +18,22 @@ def factor_sig(n):
     sig = {}
     factors_s = re.sub('^[^:]*:', '', l)
     for x in re.findall('[0-9]+', factors_s):
-        if not x in sig:
+        if x not in sig:
             sig[x] = 0
         sig[x] += 1
     return sorted(sig.values())
 
+
 def check_factor_sig(n, good):
     if factor_sig(n) != good:
-        print ("%d is not good" % (n));
+        print("%d is not good" % (n))
         raise BaseException
     return
 
-check_factor_sig(24, [1, 3]);
-check_factor_sig(100, [2, 2]);
-check_factor_sig(1000, [3, 3]);
+check_factor_sig(24, [1, 3])
+check_factor_sig(100, [2, 2])
+check_factor_sig(1000, [3, 3])
+
 
 # sig must be sorted.
 def real_calc_num_chains(sig):
@@ -41,23 +43,28 @@ def real_calc_num_chains(sig):
         ret = 0
         n = sig[x]
         for c in xrange(0, n+1):
-            ret += helper(so_far + [n - c] if c < n else so_far, x+1, (all_zeros and (c == 0)))
+            ret += helper(so_far + [n - c] if c < n else so_far,
+                          x+1,
+                          (all_zeros and (c == 0)))
         return ret
     return helper([], 0, True)
+
 
 # sig must be sorted.
 def calc_num_chains(sig):
     sig_s = ','.join(str(x) for x in sig)
-    if not sig_s in cache:
+    if sig_s not in cache:
         cache[sig_s] = real_calc_num_chains(sig)
     return cache[sig_s]
+
 
 def calc_g(n):
     return calc_num_chains(factor_sig(n))
 
+
 def check_num_chains(n, good):
     if calc_g(n) != good:
-        print ("calc_num_chains %d is not good" % (n));
+        print("calc_num_chains %d is not good" % (n))
         raise BaseException
     return
 
@@ -70,9 +77,11 @@ LIM = long('1' + ('0' * 16))
 found = set()
 found.add(long(1))
 
+
 def iter_over_sigs(length):
     if calc_num_chains([1] * length) > LIM:
         return False
+
     def helper(so_far):
         if len(so_far) == length:
             ret = calc_num_chains(list(reversed(so_far)))
@@ -81,7 +90,7 @@ def iter_over_sigs(length):
             if (ret == calc_g(ret)):
                 found.add(ret)
             return True
-        for x in xrange(1,so_far[-1]+1):
+        for x in xrange(1, so_far[-1]+1):
             if not helper(so_far + [x]):
                 if x == 1:
                     return False
@@ -95,7 +104,7 @@ def iter_over_sigs(length):
 
 length = 1
 while (iter_over_sigs(length)):
-    print ("Finished len = %d" % (length))
+    print("Finished len = %d" % (length))
     length += 1
 
-print ("Result = %d" % (sum(found)))
+print("Result = %d" % (sum(found)))
