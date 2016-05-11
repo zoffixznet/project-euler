0269d96fc1d84097324d27b7f74b610dd8894824
Shlomi Fish <shlomif@shlomifish.org>
2016-05-11 16:06:47 +0300

    Add more - still mostly untested.

--- project-euler/424/Euler424_v1.pm
+++ project-euler/424/Euler424_v1.pm
@@ -3,6 +3,60 @@ package Euler424_v1;
 use strict;
 use warnings;
 
+package Euler424_v1::Hint;
+
+use Moose;
+
+has 'sum' => (is => 'rw', isa => 'Str');
+has 'affected_cells' => (is => 'rw', isa => 'ArrayRef',
+    default => sub { return []; });
+
+package Euler424_v1::Cell;
+
+use Moose;
+
+has 'gray' => (is => 'rw', isa => 'Bool');
+has ['horiz_hint', 'vert_hint'] => (is => 'rw', 'isa' => 'Maybe[Euler424_v1::Hint]');
+has 'digit' => (is => 'rw', isa => 'Maybe[Str]');
+has ['horiz_affecting_sum', 'vert_affecting_sum'] => (is => 'rw', 'isa' => 'Maybe[ArrayRef]');
+
+sub set_gray
+{
+    my ($self) = @_;
+
+    $self->gray(1);
+    $self->horiz_hint(undef());
+    $self->vert_hint(undef());
+    $self->digit(undef());
+
+    return;
+}
+
+sub set_digit
+{
+    my ($self,$val) = @_;
+
+    $self->gray(0);
+    $self->digit(length$val ? $val : undef());
+
+    return;
+}
+
+sub set_hints
+{
+    my ($self,$args) = @_;
+
+    $self->gray(1);
+    my $h = $args->{h};
+    $self->horiz_hint(defined($h) ? Euler424_v1::Hint->new({ sum => $h }) : $h);
+
+    my $v = $args->{v};
+    $self->vert_hint(defined($v) ? Euler424_v1::Hint->new({ sum => $v }) : $v);
+
+    return;
+}
+
+
 package Euler424_v1::Puzzle;
 
 use Moose;
@@ -16,5 +70,61 @@ my $NUM_DIGITS = 10;
 has 'height' => (is => 'ro', isa => 'Int');
 has 'width' => (is => 'ro', isa => 'Int');
 has 'truth_table' => (is => 'rw', default => sub { return [map { [($EMPTY) x $NUM_DIGITS]} (1 .. $NUM_DIGITS)]; });
+has 'grid' => (is => 'rw', default => sub {
+        my $self = shift;
+        return [map { [map { Euler424_v1::Cell->new; } 1 .. $self->width] }
+            1 .. $self->height
+        ]
+    }
+);
+
+sub cell
+{
+    my ($self,$y,$x) = @_;
+
+    return $self->grid->[$y]->[$x];
+}
+
+sub populate_from_string
+{
+    my ($self,$s) = @_;
+
+    $s =~ s#\r?\n?\z#,#;
+
+    foreach my $y (0 .. $self->height - 1)
+    {
+        foreach my $x (0 .. $self->width - 1)
+        {
+            if ($s =~ s#\AX,##)
+            {
+                $self->cell($y,$x)->set_gray;
+            }
+            elsif ($s =~ s#\AO,##)
+            {
+                $self->cell($y, $x)->set_digit('');
+            }
+            elsif ($s =~ s#\A([A-J]),##)
+            {
+                $self->cell($y, $x)->set_digit($1);
+            }
+            elsif ($s =~ s#\A\((?:h([A-J]{1,2}))?,?v([A-J]{1,2})?\),##)
+            {
+                $self->cell($y, $x)->set_hints({ h => ($1 || undef()), v => ($2 || undef())});
+            }
+            else
+            {
+                die "Unknown format for string <<$s>>!";
+            }
+        }
+    }
+
+    if ($s ne '')
+    {
+        die "Junk in line - <<$s>>!";
+    }
+    # TODO: cross-reference the sums and the hints.
+    return;
+}
+
 
 1;
