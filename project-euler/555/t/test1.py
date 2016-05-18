c9a2d2807d49f1257a6617bc3b63ecc6296dd4a3
Shlomi Fish <shlomif@shlomifish.org>
2016-05-18 22:00:14 +0300

    flake8 compliance

--- project-euler/555/t/test1.py
+++ project-euler/555/t/test1.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 
 import sys
-import os
-
-sys.path.insert(0, '.')
-
-from TAP.Simple import *
-
+from TAP.Simple import diag, is_ok, ok, plan
 from euler555 import M_func, S_func
 
-plan( 6 )
+if sys.version_info > (3,):
+    long = int
+    xrange = range
 
-def eq_ok(have,want,blurb):
+plan(6)
+
+
+def eq_ok(have, want, blurb):
     ret = ok(have == want, blurb)
     if (not ret):
         diag("(have = '%s', want = '%s')" % (have, want))
     return ret
 
+
 def main():
-    M_91 = M_func(100,11,10)
+    M_91 = M_func(100, 11, 10)
 
     # TEST
     is_ok(M_91.calc(101), 91, "M[91](101) == 91")
@@ -27,19 +28,17 @@ def main():
     is_ok(M_91.calc(91), 91, "M[91](91) == 91")
 
     # TEST
-    ok(M_91.calc_F() == [91], "calc_F");
+    ok(M_91.calc_F() == [91], "calc_F")
 
     # TEST
-    ok(M_91.calc_SF() == long(91), "calc_SF");
+    ok(M_91.calc_SF() == long(91), "calc_SF")
 
     # TEST
-    eq_ok(S_func(10, 10).calc(), long(225), "S(10,10)");
+    eq_ok(S_func(10, 10).calc(), long(225), "S(10,10)")
 
     # TEST
-    eq_ok(S_func(1000, 1000).calc(), long(208724467), "S(1000,1000)");
+    eq_ok(S_func(1000, 1000).calc(), long(208724467), "S(1000,1000)")
 
-#----------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
-
