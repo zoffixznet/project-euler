ca7065b4497f9e338fee43f2d90c05d41d920ffa
Shlomi Fish <shlomif@shlomifish.org>
2016-05-27 19:34:47 +0300

    Add the correct solution for #286

---
+++ project-euler/286/euler_286_v1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+
+def calc(q):
+    probs = [1.0]
+    for x in xrange(1, 51):
+        p = 1 - x / q
+        new_probs = []
+        for i in xrange(0, x+1):
+            # print("%d %d" % (i,x))
+            new_probs.append((0 if (i == x) else probs[i] * (1-p)) +
+                             (0 if (i == 0) else (p * probs[i-1])))
+        probs = new_probs
+    return probs[20]
+
+l = float(50)
+h = float(100000)
+
+while True:
+    m = ((l + h) / 2)
+    v_m = calc(m)
+    print("%.40f = %.40f" % (m, v_m))
+    if v_m > 0.02:
+        l = m
+    elif v_m < 0.02:
+        h = m
+    else:
+        break
