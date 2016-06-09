a6b381b2a90804ec0380c4c4f39258ac14c63612
Shlomi Fish <shlomif@shlomifish.org>
2016-06-09 15:35:13 +0300

    Add a partial solution to Euler #267.

---
+++ project-euler/267/euler_267_v1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import math
+from bigfloat import *
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+with precision(20000):
+    N_tosses = 1000
+    h_probs = [math.factorial(N_tosses)/(math.factorial(x)*math.factorial(N_tosses-x))*(BigFloat('0.5') ** N_tosses) for x in xrange(0, N_tosses+1)]
+    s = 0
+    h_sums = [0]
+    for x in h_probs:
+        s += x
+        h_sums.append(s + 0)
+
+
+    def calc_prob(f, h_sums):
+        h = N_tosses + 0
+        money = (1 + 2 * f) ** h
+        while money > 1e9:
+            h -= 1
+            money *= (1 - f) / (1 + 2 * f)
+        h += 1
+        return h_sums[N_tosses + 1 - h]
+
+    for f_base in xrange(0, 101):
+        print (("f=%d%% prob = %.16f") % (f_base, calc_prob(f_base * BigFloat('0.01'), h_sums)))
