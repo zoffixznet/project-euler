6e870f782a9436f3afabcac1541f7d300577b8e4
Shlomi Fish <shlomif@shlomifish.org>
2016-06-14 18:05:15 +0300

    Implement s_smart which seems to be working nicely
    Hopefully it will allow us to solve the problem.

--- project-euler/463/euler-463-v1.pl
+++ project-euler/463/euler-463-v1.pl
@@ -37,9 +37,58 @@ sub f_mod
     }->();
 }
 
-my $s = 0;
-foreach my $n (1 .. 1_000_000_000)
+sub s_bruteforce
 {
-    ($s += f_mod($n)) %= (1_000_000_000);
-    say $s;
+    my ($n) = @_;
+
+    my $s = 0;
+
+    foreach my $i (1 .. $n)
+    {
+        ($s += f_mod($i)) %= $MOD;
+    }
+
+    return $s;
+}
+
+{
+    my %s_cache;
+
+    sub s_smart
+    {
+        my ($start, $end) = @_;
+
+        return $s_cache{"$start|$end"} //= sub {
+            if ($start > $end)
+            {
+                return 0;
+            }
+            if ($end <= 8)
+            {
+                return s_bruteforce($end) - s_bruteforce($start - 1);
+            }
+            if (($start & 0b11) != 0)
+            {
+                return ((f_mod($start) + s_smart($start+1, $end)) % $MOD);
+            }
+            if (($end & 0b11) != 11)
+            {
+                return ((f_mod($end) + s_smart($start, $end-1)) % $MOD);
+            }
+            my $half_start = ($start >> 1);
+            my $half_end = (($end - 1) >> 1);
+            return ((6 * f_mod($half_end) + (s_smart($half_start+1, $half_end-1) << 2) - (f_mod($half_start) << 1)) % $MOD);
+        }->();
+    }
+}
+
+my $want = 0;
+foreach my $n (1 .. 1_000_000)
+{
+    ($want += f_mod($n)) %= $MOD;
+    my $have = s_smart(1, $n);
+    if ($want != $have)
+    {
+        die "want=$want have=$have n=$n!";
+    }
 }
