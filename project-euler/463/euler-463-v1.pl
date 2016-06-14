287d67ba9e924888c87c13a10eb9394ff4f01ec5
Shlomi Fish <shlomif@shlomifish.org>
2016-06-14 16:51:43 +0300

    Add more

--- project-euler/463/euler-463-v1.pl
+++ project-euler/463/euler-463-v1.pl
@@ -7,33 +7,39 @@ use 5.016;
 
 my $MOD = 1_000_000_000;
 
+my %cache;
+
 sub f_mod
 {
     my ($n) = @_;
 
-    if ($n == 1)
-    {
-        return 1;
-    }
-    elsif ($n == 3)
-    {
-        return 3;
-    }
-    elsif (($n & 1) == 0)
-    {
-        return f_mod($n >> 1);
-    }
-    elsif (($n & 3) == 1)
-    {
-        return (2 * f_mod(($n >> 1) + 1) - f_mod($n >> 2)) % $MOD;
-    }
-    else
-    {
-        return (3 * f_mod(($n >> 1)) - 2 * f_mod($n >> 2)) % $MOD;
-    }
+    return $cache{$n} //= sub {
+        if ($n == 1)
+        {
+            return 1;
+        }
+        elsif ($n == 3)
+        {
+            return 3;
+        }
+        elsif (($n & 1) == 0)
+        {
+            return f_mod($n >> 1);
+        }
+        elsif (($n & 3) == 1)
+        {
+            return (2 * f_mod(($n >> 1) + 1) - f_mod($n >> 2)) % $MOD;
+        }
+        else
+        {
+            return (3 * f_mod(($n >> 1)) - 2 * f_mod($n >> 2)) % $MOD;
+        }
+    }->();
 }
 
+my $s = 0;
 foreach my $n (1 .. 1_000_000_000)
 {
-    say f_mod($n);
+    ($s += f_mod($n)) %= (1_000_000_000);
+    say $s;
 }
