4c5f3bc5f4a5933567c2b0c45bfa3201373d6d0f
Shlomi Fish <shlomif@shlomifish.org>
2016-06-14 18:59:24 +0300

    This program should work but panics.
    This program should work but instead causes a panic in perl.

--- project-euler/463/euler-463-v1.pl
+++ project-euler/463/euler-463-v1.pl
@@ -5,7 +5,37 @@ use warnings;
 
 use 5.016;
 
+no warnings 'recursion';
+
 my $MOD = 1_000_000_000;
+
+my $UPPER = 200_000;
+my $LOWER = 100_000;
+
+sub _cache
+{
+    my ($h, $key, $promise) = @_;
+
+    my $ret = ($h->{$key} //= $promise->());
+
+    if (scalar keys %$h >= $UPPER)
+    {
+        my @to_del;
+
+        my $NUM = +(scalar keys %$h) - $LOWER;
+        K:
+        while (my ($k, undef) = each %$h)
+        {
+            push @to_del, $k;
+            if (@to_del == $NUM)
+            {
+                last K;
+            }
+        }
+        delete @$h{@to_del};
+    }
+    return $ret;
+}
 
 my %cache;
 
@@ -13,7 +43,7 @@ sub f_mod
 {
     my ($n) = @_;
 
-    return $cache{$n} //= sub {
+    return _cache(\%cache, $n, sub {
         if ($n == 1)
         {
             return 1;
@@ -34,7 +64,7 @@ sub f_mod
         {
             return (3 * f_mod(($n >> 1)) - 2 * f_mod($n >> 2)) % $MOD;
         }
-    }->();
+    });
 }
 
 sub s_bruteforce
@@ -58,37 +88,45 @@ sub s_bruteforce
     {
         my ($start, $end) = @_;
 
-        return $s_cache{"$start|$end"} //= sub {
-            if ($start > $end)
-            {
-                return 0;
+        return _cache(\%s_cache, "$start|$end", sub {
+                if ($start > $end)
+                {
+                    return 0;
+                }
+                if ($end <= 8)
+                {
+                    return s_bruteforce($end) - s_bruteforce($start - 1);
+                }
+                if (($start & 0b11) != 0)
+                {
+                    return ((f_mod($start) + s_smart($start+1, $end)) % $MOD);
+                }
+                if (($end & 0b11) != 11)
+                {
+                    return ((f_mod($end) + s_smart($start, $end-1)) % $MOD);
+                }
+                my $half_start = ($start >> 1);
+                my $half_end = (($end - 1) >> 1);
+                return ((6 * f_mod($half_end) + (s_smart($half_start+1, $half_end-1) << 2) - (f_mod($half_start) << 1)) % $MOD);
             }
-            if ($end <= 8)
-            {
-                return s_bruteforce($end) - s_bruteforce($start - 1);
-            }
-            if (($start & 0b11) != 0)
-            {
-                return ((f_mod($start) + s_smart($start+1, $end)) % $MOD);
-            }
-            if (($end & 0b11) != 11)
-            {
-                return ((f_mod($end) + s_smart($start, $end-1)) % $MOD);
-            }
-            my $half_start = ($start >> 1);
-            my $half_end = (($end - 1) >> 1);
-            return ((6 * f_mod($half_end) + (s_smart($half_start+1, $half_end-1) << 2) - (f_mod($half_start) << 1)) % $MOD);
-        }->();
+        );
     }
 }
 
-my $want = 0;
-foreach my $n (1 .. 1_000_000)
+if (0)
 {
-    ($want += f_mod($n)) %= $MOD;
-    my $have = s_smart(1, $n);
-    if ($want != $have)
+    my $want = 0;
+    foreach my $n (1 .. 1_000_000)
     {
-        die "want=$want have=$have n=$n!";
+        ($want += f_mod($n)) %= $MOD;
+        my $have = s_smart(1, $n);
+        if ($want != $have)
+        {
+            die "want=$want have=$have n=$n!";
+        }
     }
+}
+
+{
+    say "S(3 ** 37) = ", s_smart(1, (eval join'*',(3) x 37));
 }
