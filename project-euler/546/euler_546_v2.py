82745ff72a221f6fa185410ef7451905ebc8dde8
Shlomi Fish <shlomif@shlomifish.org>
2016-06-30 22:47:59 +0300

    add some experimental code.

--- project-euler/546/euler_546_v2.py
+++ project-euler/546/euler_546_v2.py
@@ -22,7 +22,7 @@ class MyIter:
     def __init__(self):
         global k
         self.n = 0
-        self.f = 1
+        self.f = long(1)
         # Remaining.
         self.r = 1
         return
@@ -41,25 +41,58 @@ class MyIter:
                 self.r = k
                 self.c.inc()
             self.f += self.c.f
-            if self.f > M:
-                self.f -= M
         return
 
     def add(self, delta):
+        global k, M
+        init_f = self.f
         target = self.n + delta
+        ret = init_f
+        while self.r != k and self.n < target:
+            self.inc()
+            ret += self.f
+        target_k = target
+        while target_k % k != 0:
+            target_k -= 1
+        if target_k > self.n:
+            k_delta = (target_k - self.n) // k
+            k_k_delta = k_delta // k
+            if k_k_delta > 0:
+                for i in range(0,k):
+                    k_ret = self.c.add(k_k_delta)
+                    self.f += k_ret * k
+                    ret += self.f
+                    self.n += k_k_delta * k
         while self.n < target:
             self.inc()
-        return
+            ret += self.f
+        # ret -= self.f
+        return ret
 
 f = MyIter()
 # STEP
-S = 1000000
+S = 1000
 # limit
 l = S
 # What we want.
-w = 100000000000000
+w = 1000
 while f.n < w:
     while f.n < l:
-        f.add(10)
+        new_f = MyIter()
+        new_f.add(f.n)
+        print ("f(%d) = %d" % (f.n, f.f))
+        print ("NEWf(%d) = %d" % (new_f.n, new_f.f))
+        f.inc()
     l += S
     print ("f(%d) = %d" % (f.n, f.f))
+
+f = MyIter()
+l = S
+while f.n < w:
+    while f.n < l:
+        f.add(1)
+    l += S
+    print ("f(%d) = %d" % (f.n, f.f))
+f = MyIter()
+f.add(1000)
+print ("f(%d) = %d" % (f.n, f.f))
