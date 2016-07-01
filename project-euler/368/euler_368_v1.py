02b792c1d0a65b7b2a72fb85c11333fa4cc174d9
Shlomi Fish <shlomif@shlomifish.org>
2016-07-01 20:45:09 +0300

    Add a slow solution.

---
+++ project-euler/368/euler_368_v1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import re
+from bigfloat import *
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+with precision(60):
+    r = re.compile('(000|111|222|333|444|555|666|777|888|999)')
+    n = 1
+    # total = BigFloat('0')
+    total = 0.0
+    step = 100000
+    checkpoint = step
+    while True:
+        n_s = str(n)
+        m = r.search(n_s)
+        if m:
+            n += int('1' + '0' * (len(n_s) - m.end(1)))
+        else:
+            # total += BigFloat('1') / n
+            total += 1.0 / n
+            while n > checkpoint:
+                print (("n=%030d t = %.40f") % (n, total))
+                sys.stdout.flush()
+                checkpoint += step
+            n += 1
