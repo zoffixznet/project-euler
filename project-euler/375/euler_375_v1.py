601bc11311a4194099d2308fc839a2ea2c75b866
Shlomi Fish <shlomif@shlomifish.org>
2016-07-19 18:42:27 +0300

    working now for the test cases.

--- project-euler/375/euler_375_v1.py
+++ project-euler/375/euler_375_v1.py
@@ -9,10 +9,22 @@ if sys.version_info > (3,):
 S_0 = 290797
 S = S_0
 
-a = []
-s = 0
+def brute(m):
+    s_s = [S_0]
+    for i in xrange(1,m+1):
+        s_s.append((s_s[-1] * s_s[-1]) % 50515093)
 
-m = 10
+    mysum = long(0)
+    for i in xrange(1,m+1):
+        for j in xrange(i,m+1):
+            mysum += min(s_s[i:(j+1)])
+
+    return mysum
+
+a = []
+s = long(0)
+
+m = 10000
 for j in xrange(1,m+1):
     S = ((S * S) % 50515093)
     n = []
@@ -25,8 +37,15 @@ for j in xrange(1,m+1):
         else:
             n.append(t)
     n.append({'i':i,'S':S})
-    a = n
-    for t in a:
-        s += t['S'] * (j-t['i']+1)
+    a = sorted(n,key=lambda (t):t['i'])
+    for t in xrange(0,len(a)):
+        bottom = (1 if t == 0 else a[t]['i'])
+        top = (j if t == len(a)-1 else a[t+1]['i']-1)
+        val = (a[t]['S'] if t < len(a) else S)
+        s += val * (top - bottom +1)
+    # s -= S
+    # if (s != brute(j)):
+      #  print("Wrong for N=%d ; good=%d bad=%d" % (j, brute(j), s))
+      #  raise RuntimeError
 
 print(("M(%d) = %d") % (m, s))
