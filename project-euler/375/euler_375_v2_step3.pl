dd2b1e41699704516e027678ec2886a3063ddde1
Shlomi Fish <shlomif@shlomifish.org>
2016-07-30 19:12:36 +0300

    abstract and fix the v2_step3 code.

--- project-euler/375/euler_375_v2_step3.pl
+++ project-euler/375/euler_375_v2_step3.pl
@@ -5,30 +5,36 @@ use warnings;
 
 use bigint;
 
-my $MIN_ELEM = 3;
+sub lookup
+{
+    my ($place) = @_;
+
+    return 0 + (`grep -F 'M($place) =' < dump_v3` =~ s/.* = //r);
+}
+
 my $delta = 6308949 - 1;
 my $s = $delta + 1;
-my $m_s = 4336411086199724;
-my $m_s2 = 9043999811195255;
+my $s2 = $s + $delta;
+my $m_s = lookup($s);
+my $m_s2 = lookup($s2);
 
 my $m_delta = $m_s2 - $m_s;
-my $s2 = $s + $delta;
 
 my $n = $s2;
 my $m = $m_s2 + 0;
+my $TARGET = $delta * 3 + 1;
 
-my $s_t = 63484;
+my $s_t = (( $TARGET - 1 ) % $delta + 1);
 my $s_t_min_1 = $s_t - 1;
 
 my $m_s2_t = $s + $s_t_min_1;
 
+my $MIN_ELEM = 3;
 print "\$s = $s\n";
 print "\$m_s2_t = $m_s2_t\n";
-my $M6308949 = 4336411086199724;
-my $M6372432 = 4390481392426100;
-my $delta_t2 = $M6372432 - $M6308949;
+my $delta_t2 = lookup($m_s2_t) - $m_s;
 $n += $delta;
-while ($n < 2_000_000_000)
+while ($n <= $TARGET)
 {
     $m_delta += $MIN_ELEM * $delta * $delta;
     $delta_t2 += $MIN_ELEM * $delta * $s_t_min_1;
@@ -39,4 +45,4 @@ continue
     $n += $delta;
 }
 print "M(@{[$n-$delta]}) = $m\n";
-print "M(@{[2_000_000_000]}) = @{[$m + $delta_t2]}\n";
+print "M(@{[$TARGET]}) = @{[$m + $delta_t2]}\n";
