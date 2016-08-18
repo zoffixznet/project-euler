e4f8f988bc202a46d77c0e8d9f589f22dde5b8c0
Shlomi Fish <shlomif@shlomifish.org>
2016-08-18 16:48:13 +0300

    Add some tests

--- project-euler/505/euler_505_v1.py
+++ project-euler/505/euler_505_v1.py
@@ -23,5 +23,23 @@ def y(n,k):
 def A(n):
     return y(n,1)
 
+def ass_y(n,k,expected):
+    got = y(n,k)
+    if (got != expected):
+        print( "y(%d,%d) = %d != %d" % (n,k,got,expected))
+        raise BaseException
+    return
 
+def ass_A(n,expected):
+    got = A(n)
+    if (got != expected):
+        print( "A(%d) = %d != %d" % (n,got,expected))
+        raise BaseException
+    return
 
+ass_y(4,4,11)
+ass_y(4,3,((1 << 60)-9))
+ass_y(4,2,((1 << 60)-12))
+ass_y(4,1,8)
+ass_A(10, ((1 << 60)-34))
+ass_A(1000, 101881)
