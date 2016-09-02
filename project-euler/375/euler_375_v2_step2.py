572f33a1c14bc5063e1cbe4c9cf80486a574c697
Shlomi Fish <shlomif@shlomifish.org>
2016-09-02 15:36:14 +0300

    flake8 compliance

--- project-euler/375/euler_375_v2_step2.py
+++ project-euler/375/euler_375_v2_step2.py
@@ -9,14 +9,15 @@ if sys.version_info > (3,):
 S_0 = 290797
 S = S_0
 
+
 def brute(m):
     s_s = [S_0]
-    for i in xrange(1,m+1):
+    for i in xrange(1, m+1):
         s_s.append((s_s[-1] * s_s[-1]) % 50515093)
 
     mysum = long(0)
-    for i in xrange(1,m+1):
-        for j in xrange(i,m+1):
+    for i in xrange(1, m+1):
+        for j in xrange(i, m+1):
             mysum += min(s_s[i:(j+1)])
 
     return mysum
@@ -29,7 +30,7 @@ s = long(0)
 m = 6308949 * 5
 st = 1
 lim = st
-for j in xrange(1,m+1):
+for j in xrange(1, m+1):
     S = ((S * S) % 50515093)
     n = []
     i = j
@@ -40,17 +41,13 @@ for j in xrange(1,m+1):
                 i = t['i']
         else:
             n.append(t)
-    n.append({'i':i,'S':S})
-    a = sorted(n,key=lambda (t):t['i'])
-    for t in xrange(0,len(a)):
+    n.append({'i': i, 'S': S})
+    a = sorted(n, key=lambda t: t['i'])
+    for t in xrange(0, len(a)):
         bottom = (1 if t == 0 else a[t]['i'])
         top = (j if t == len(a)-1 else a[t+1]['i']-1)
         val = (a[t]['S'] if t < len(a) else S)
-        s += val * (top - bottom +1)
-    # s -= S
-    # if (s != brute(j)):
-      #  print("Wrong for N=%d ; good=%d bad=%d" % (j, brute(j), s))
-      #  raise RuntimeError
+        s += val * (top - bottom + 1)
     if j == lim:
         lim += st
         print(("M(%d) = %d") % (j, s))
