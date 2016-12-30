fb5d94dac260569000a89dbf9e4374a24c4ab4b3
Shlomi Fish <shlomif@shlomifish.org>
2016-12-30 18:54:50 +0200

    Fix some bugs - now it yields the correct sol.
    But it's slow.

--- project-euler/322/euler_322_v1.py
+++ project-euler/322/euler_322_v1.py
@@ -1,4 +1,6 @@
+import math
 import sys
+
 if sys.version_info > (3,):
     long = int
     xrange = range
@@ -36,31 +38,37 @@ class Powers:
         self.i += 1
         return r
 
+def C(i,n):
+    return (math.factorial(i) / (math.factorial(n) * math.factorial(i-n)))
 
-
-def main():
-    m = long('1' + '0' * 9)
-    n = long(10000000 - 10)
-
-    numer = [Powers(long(2), m, n), Powers(long(5), m, n)]
+def T(m, n):
+    numer = [Powers(long(2), m, n+1), Powers(long(5), m, n+1)]
     denom = [Powers(long(2), m, 1), Powers(long(5), m, 1)]
     s = [0,0]
     ret = 0
+    verd = False
     for i in range(n, m):
+        if verd:
+            ret += 1
         verd = True
         for j in [0,1]:
             s[j] += numer[j].calc() - denom[j].calc()
             if s[j] < 1:
                 verd = False
-        if verd:
-            ret += 1
+        if False:
+            real_verd = (C(i+1,n) % 10 == 0)
+            if real_verd != verd:
+                raise BaseException( "real_verd=%s verd=%s i=%d" % (real_verd, verd, i))
         if i % 1000000 == 0:
             print("i = %d ; ret = %d" % (i, ret))
             sys.stdout.flush()
+    return ret
 
-    print("ret = %d" % (ret))
-
-    return
+def main():
+    print("ret = %d" % (T(100,5)))
+    m = long('1' + '0' * 9)
+    n = long(10000000 - 10)
+    print("ret = %d" % (T(m,n)))
 
 if __name__ == "__main__":
     main()
