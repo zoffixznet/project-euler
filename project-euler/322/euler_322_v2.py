1f81438a01d3a599909adda9a0323b9dc693f740
Shlomi Fish <shlomif@shlomifish.org>
2017-01-07 18:10:13 +0200

    Optimisation.

--- project-euler/322/euler_322_v2.py
+++ project-euler/322/euler_322_v2.py
@@ -72,10 +72,10 @@ def base_iter(it):
 
 class IterWrap:
     """docstring for IterWrap"""
-    def __init__(self, b, n):
+    def __init__(self, b, n, d):
         self.b, self.n = b, n
-        self.data = calc_base_iter(b, n)
-        self.it = base_iter(self.data)
+        self.d = d
+        self.it = base_iter(self.d)
         self.c = 0
         self.next()
         return;
@@ -88,8 +88,8 @@ class IterWrap:
 
     def skip_to(self, m):
         """docstring for skip_to"""
-        p = self.data['power']
-        count_per_p = reduce((lambda a,b: a*b), (len(x) for x in self.data['digits']))
+        p = self.d['power']
+        count_per_p = reduce((lambda a,b: a*b), (len(x) for x in self.d['digits']))
         num = long((m - self.i) / p)
         delta = num * p
         self.i += delta
@@ -100,11 +100,12 @@ class IterWrap:
         return
 
 def calc_common(m, n):
-    i2 = IterWrap(2, n)
+    i2 = IterWrap(2, n, calc_base_iter(2, n))
     ret = 0
-    p2 = i2.data['power']
-    i5 = IterWrap(5, n)
-    p5 = i5.data['power']
+    p2 = i2.d['power']
+    d = calc_base_iter(5, n)
+    i5 = IterWrap(5, n, d)
+    p5 = d['power']
     p = p2 * p5
     a = (p, [p2,p5], [p5,p2])
     m_i_s = [mul_inv(p_i, n_i) * p_i for n_i, p_i in zip(a[1], a[2])]
@@ -113,7 +114,7 @@ def calc_common(m, n):
     while i2.i < p2:
         print i2.i, "/", p2
         j2 = i2.i * m2
-        i5 = IterWrap(5, n)
+        i5 = IterWrap(5, n, d)
         while i5.i < p5:
             mod_ = ((j2 + i5.i * m5) % p)
             while mod_ < m:
@@ -124,7 +125,7 @@ def calc_common(m, n):
     return ret
 
 def T(m, n):
-    i = [IterWrap(2, n), IterWrap(5, n)]
+    i = [IterWrap(2, n, calc_base_iter(2, n)), IterWrap(5, n, calc_base_iter(5,n))]
     i[0].skip_to(m)
     i[1].skip_to(m)
     c_calc = calc_common(m,n)
