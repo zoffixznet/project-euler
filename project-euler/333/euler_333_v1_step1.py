e6e4152e0ed666a0b84d4aabe738adcfd7cb4e2a
Shlomi Fish <shlomif@shlomifish.org>
2017-01-16 02:02:12 +0200

    flake8.

--- project-euler/333/euler_333_v1_step1.py
+++ project-euler/333/euler_333_v1_step1.py
@@ -7,21 +7,22 @@ if sys.version_info > (3,):
     xrange = range
 
 L = int(sys.argv.pop(1))
-powers = {2: [1], 3: [1],}
+powers = {2: [1], 3: [1]}
 
-for e in [2,3]:
+for e in [2, 3]:
     while powers[e][-1] < 1000000:
         powers[e].append(powers[e][-1]*e)
 
-p = [[powers[3][i3]*powers[2][i2] for i3 in xrange(0,len(powers[3]))] for i2 in xrange(0,len(powers[2]))]
+p = [[powers[3][i3]*powers[2][i2] for i3 in xrange(0, len(powers[3]))]
+     for i2 in xrange(0, len(powers[2]))]
 for x3 in [len(powers[3])-1]:
     def rec(x, start_y, mysum):
         """docstring for rec"""
         if x == -1:
             print(mysum)
         else:
-            rec(x-1,start_y, mysum)
-            for y in xrange(start_y,len(powers[2])):
+            rec(x-1, start_y, mysum)
+            for y in xrange(start_y, len(powers[2])):
                 s = mysum + p[y][x]
                 if s >= L:
                     return
--- project-euler/333/euler_333_v1_total.py
+++ project-euler/333/euler_333_v1_total.py
@@ -7,7 +7,8 @@ if sys.version_info > (3,):
     long = int
     xrange = range
 
-pipe = subprocess.Popen(['primes', '2', '1000000'], shell=False, stdout=subprocess.PIPE)
+pipe = subprocess.Popen(['primes', '2', '1000000'], shell=False,
+                        stdout=subprocess.PIPE)
 primes = {}
 l = pipe.stdout.readline()
 while l:
@@ -15,20 +16,23 @@ while l:
     l = pipe.stdout.readline()
 
 L = int(sys.argv.pop(1))
-powers = {2: [1], 3: [1],}
+powers = {2: [1], 3: [1]}
 
-for e in [2,3]:
+for e in [2, 3]:
     while powers[e][-1] < 1000000:
         powers[e].append(powers[e][-1]*e)
 
-p = [[powers[3][i3]*powers[2][i2] for i3 in xrange(0,len(powers[3]))] for i2 in xrange(0,len(powers[2]))]
+p = [[powers[3][i3]*powers[2][i2] for i3 in xrange(0, len(powers[3]))]
+     for i2 in xrange(0, len(powers[2]))]
+
+
 def rec(x, start_y, mysum):
     if x == -1:
         if mysum in primes:
             primes[mysum] += 1
     else:
-        rec(x-1,start_y, mysum)
-        for y in xrange(start_y,len(powers[2])):
+        rec(x-1, start_y, mysum)
+        for y in xrange(start_y, len(powers[2])):
             s = mysum + p[y][x]
             if s >= L:
                 return
