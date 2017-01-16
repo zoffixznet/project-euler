6a2b872590ec48b0caf729f6c6cb1b59069dc313
Shlomi Fish <shlomif@shlomifish.org>
2017-01-16 02:40:41 +0200

    flake8.

--- project-euler/322/euler_322_v2.py
+++ project-euler/322/euler_322_v2.py
@@ -1,5 +1,5 @@
-import math
 import sys
+import functools
 
 if sys.version_info > (3,):
     long = int
@@ -10,7 +10,7 @@ if sys.version_info > (3,):
 # Taken from http://rosettacode.org/wiki/Chinese_remainder_theorem#Python -
 # thanks!
 # Python 2.7
-def chinese_remainder((prod, n, p_n), a):
+def chinese_remainder(prod, n, p_n, a):
     sum = 0
     for n_i, p, a_i in zip(n, p_n, a):
         sum += a_i * mul_inv(p, n_i) * p
@@ -20,23 +20,27 @@ def chinese_remainder((prod, n, p_n), a):
 def mul_inv(a, b):
     b0 = b
     x0, x1 = 0, 1
-    if b == 1: return 1
+    if b == 1:
+        return 1
     while a > 1:
         q = a / b
-        a, b = b, a%b
+        a, b = b, a % b
         x0, x1 = x1 - q * x0, x0
-    if x1 < 0: x1 += b0
+    if x1 < 0:
+        x1 += b0
     return x1
 
 # End of Rosetta code.
 # >>>>>>>>>>>>>>>>>>
 
-def mydiv(i,e):
-    ret = (i/e)*e;
+
+def mydiv(i, e):
+    ret = (i/e)*e
     if ret < i:
         return ret + e
     else:
         return ret
+
 
 def calc_base_iter(b, n):
     """docstring for calc_base_iter"""
@@ -50,25 +54,28 @@ def calc_base_iter(b, n):
         else:
             digits.append([i*power for i in xrange(d, b)])
         power *= b
-    return {'base_sum': base_sum, 'b': b, 'digits': digits[::-1], 'power': power}
+    return {'base_sum': base_sum, 'b': b, 'digits': digits[::-1],
+            'power': power}
+
 
 def base_iter(it):
     base_sum = it['base_sum']
-    b = it['b']
     d = it['digits']
     p = it['power']
+
     def f(s, i):
         if i == len(d):
             yield s
         else:
             for x in d[i]:
-                for y in f(s+x,i+1):
+                for y in f(s+x, i+1):
                     yield y
     s = base_sum
     while True:
         for x in f(s, 0):
             yield x
         s += p
+
 
 class IterWrap:
     """docstring for IterWrap"""
@@ -78,7 +85,7 @@ class IterWrap:
         self.it = base_iter(self.d)
         self.c = 0
         self.next()
-        return;
+        return
 
     def next(self):
         """docstring for next"""
@@ -89,7 +96,8 @@ class IterWrap:
     def skip_to(self, m):
         """docstring for skip_to"""
         p = self.d['power']
-        count_per_p = reduce((lambda a,b: a*b), (len(x) for x in self.d['digits']))
+        count_per_p = functools.reduce(
+            (lambda a, b: a*b), (len(x) for x in self.d['digits']))
         num = long((m - self.i) / p)
         delta = num * p
         self.i += delta
@@ -99,6 +107,7 @@ class IterWrap:
             self.c += 1
         return
 
+
 def calc_common(m, n):
     i2 = IterWrap(2, n, calc_base_iter(2, n))
     ret = 0
@@ -107,12 +116,12 @@ def calc_common(m, n):
     i5 = IterWrap(5, n, d)
     p5 = d['power']
     p = p2 * p5
-    a = (p, [p2,p5], [p5,p2])
+    a = (p, [p2, p5], [p5, p2])
     m_i_s = [mul_inv(p_i, n_i) * p_i for n_i, p_i in zip(a[1], a[2])]
     m2 = m_i_s[0]
     m5 = m_i_s[1]
     while i2.i < p2:
-        print i2.i, "/", p2
+        print(i2.i, "/", p2)
         j2 = i2.i * m2
         i5 = IterWrap(5, n, d)
         while i5.i < p5:
@@ -124,17 +133,21 @@ def calc_common(m, n):
         i2.next()
     return ret
 
+
 def T(m, n):
-    i = [IterWrap(2, n, calc_base_iter(2, n)), IterWrap(5, n, calc_base_iter(5,n))]
+    i = [IterWrap(2, n, calc_base_iter(2, n)),
+         IterWrap(5, n, calc_base_iter(5, n))]
     i[0].skip_to(m)
     i[1].skip_to(m)
-    c_calc = calc_common(m,n)
-    print "c_calc = %d" % (c_calc)
+    c_calc = calc_common(m, n)
+    print("c_calc = %d" % (c_calc))
     return (m - n - (i[0].c - 1 + i[1].c - 1 - c_calc))
 
+
 def print_T(m, n):
-    print("T( m = %d, n = %d) = %d" % (m,n, T(m,n)))
+    print("T( m = %d, n = %d) = %d" % (m, n, T(m, n)))
     return
+
 
 def main():
     # print_T(100,5)
