7b84b20a48ea3da7ce47a9cb5d0ffa2239a6e73b
Shlomi Fish <shlomif@shlomifish.org>
2017-02-15 22:32:52 +0200

    Fix flake8 tests.

--- project-euler/588/euler-588-v1-trial.py
+++ project-euler/588/euler-588-v1-trial.py
@@ -14,8 +14,9 @@ def dice(num_faces, n, prev):
                 for j, v in enumerate(prev):
                     ret[i+j] ^= v
         ret = [prev[0]]
-        for j in xrange(1,len(prev)+num_faces-1):
-            ret.append(ret[-1] ^ (prev[j] if j < len(prev) else 0) ^ (prev[j-num_faces] if j >= num_faces else 0))
+        for j in xrange(1, len(prev)+num_faces-1):
+            ret.append(ret[-1] ^ (prev[j] if j < len(prev) else 0)
+                       ^ (prev[j-num_faces] if j >= num_faces else 0))
         return ret
 
 
@@ -24,6 +25,7 @@ def main():
     for n in xrange(1, 27637+1):
         this = dice(5, n, this)
         print("%d\t%d" % (n, this.count(1)))
+
 
 if __name__ == "__main__":
     main()
