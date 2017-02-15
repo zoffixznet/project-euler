c94f099a53b80a3c1bfad8c2cfdbe94067c28b34
Shlomi Fish <shlomif@shlomifish.org>
2017-02-15 16:34:02 +0200

    Some optimisations to the py version.
    Still very slow.

--- project-euler/581/euler_581_v2.py
+++ project-euler/581/euler_581_v2.py
@@ -7,7 +7,7 @@ if sys.version_info > (3,):
 
 def main():
     primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, ]
-    t = AVLTree([(long(x), True) for x in primes])
+    t = AVLTree([(x, True) for x in primes])
     prev = 1
     s = 0
     while True:
@@ -18,9 +18,10 @@ def main():
             print("Found s = %d for n = %d" % (s, prev))
             sys.stdout.flush()
         for x in primes:
-            mul = x * n
-            if not t.get_node(mul):
-                t.insert(mul, True)
+            # mul = x * n
+            # if not t.get_node(mul):
+            # t.insert(mul, True)
+            t.insert(x * n, True)
         prev = n
 
 
