00c7057552122e89a2fcfc494d8f90620445654b
Shlomi Fish <shlomif@shlomifish.org>
2017-02-15 21:47:02 +0200

    Add the solution to Euler#588.
    It requires some mix-and-match and based on
    http://oeis.org/A247649 .

--- project-euler/588/euler-588-v1-trial.py
+++ project-euler/588/euler-588-v1-trial.py
@@ -8,19 +8,22 @@ def dice(num_faces, n, prev):
     if n == 1:
         return [1] * num_faces
     else:
-        ret = [0] * (1 + num_faces * n)
-        for i in xrange(0, num_faces):
-            for j, v in enumerate(prev):
-                ret[i+j] ^= v
+        if False:
+            ret = [0] * (1 + num_faces * n)
+            for i in xrange(0, num_faces):
+                for j, v in enumerate(prev):
+                    ret[i+j] ^= v
+        ret = [prev[0]]
+        for j in xrange(1,len(prev)+num_faces-1):
+            ret.append(ret[-1] ^ (prev[j] if j < len(prev) else 0) ^ (prev[j-num_faces] if j >= num_faces else 0))
         return ret
 
 
 def main():
     this = []
-    for n in xrange(1, 300):
+    for n in xrange(1, 27637+1):
         this = dice(5, n, this)
-        print("%d\t%d" % (n, len([x for x in this if x % 2 == 1])))
-
+        print("%d\t%d" % (n, this.count(1)))
 
 if __name__ == "__main__":
     main()
---
+++ project-euler/588/total.bash
@@ -0,0 +1,1 @@
+perl -E 'say join("\t", map { oct("0b$_") } split/0{2,}/,(sprintf"%b", "1" . 0 x $_) =~ s#0+$##r) for 1 .. 18' | perl -lanE 'use IO::All qw/ io /; BEGIN{%h=(map { split } io->file("db")->getlines);}say join"\t",map{$h{$_}}@F' | perl -n -0777 -E 's#\t#*#g;s#\n#+#g;say eval$_.0'
