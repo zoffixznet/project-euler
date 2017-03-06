ec91ad954d1a5e2be4b028e1fd4334127ad8acf1
Shlomi Fish <shlomif@shlomifish.org>
2017-03-06 15:18:02 +0200

    Add S2.txt generation.

---
+++ project-euler/593/S2.py
@@ -0,0 +1,25 @@
+import sys
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+
+def main():
+    STEP = 10000
+    with open('S1.txt') as in_:
+        with open('S2.txt', 'w') as out_:
+            with open('S1.txt') as in10:
+                k = 1
+                max_k = STEP
+                S_k10 = int(in10.readline())
+                for l in in_:
+                    out_.write('%d\n' % (int(l) + S_k10))
+                    k += 1
+                    if k == max_k:
+                        max_k += STEP
+                        S_k10 = int(in10.readline())
+
+
+if __name__ == "__main__":
+    main()
