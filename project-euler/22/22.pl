8e9383f6f213ef32c96186ab50a518c4b140e1ea
Shlomi Fish <shlomif@shlomifish.org>
2017-03-31 01:45:03 +0300

    Modernize.

--- project-euler/22/22.pl
+++ project-euler/22/22.pl
@@ -11,10 +11,9 @@ my $text = io("names.txt")->slurp();
 my @names = sort { $a cmp $b } ( lc($text) =~ m{"([a-z]+)"}g );
 
 my $sum = 0;
-for my $i ( 0 .. $#names )
+while ( my ( $i, $s ) = each @names )
 {
-    $sum += ( $i + 1 ) *
-        sum( map { ord($_) - ord("a") + 1 } split( //, $names[$i] ) );
+    $sum += ( $i + 1 ) * sum( map { ord($_) - ord("a") + 1 } split( //, $s ) );
 }
 
 print "$sum\n";
