0d698114ff0db5d6b7f17229303e9bbe3f35b25a
Shlomi Fish <shlomif@shlomifish.org>
2017-04-09 15:06:18 +0300

    More progress.

--- project-euler/598/euler_598_v1.py
+++ project-euler/598/euler_598_v1.py
@@ -5,10 +5,17 @@ if sys.version_info > (3,):
     long = int
     xrange = range
 
+def find_exp(n, p, m):
+    if m > n:
+        return 0
+    else:
+        return (int(n / m) + find_exp(n, p, m*p))
 
 def calc_C(fact_n):
     primes = [x for x in xrange(2, fact_n+1) if len([y for y in xrange(2,1+int(math.sqrt(x))) if x%y == 0]) == 0]
     print(primes)
+    exps = [find_exp(fact_n, p, p) for p in primes]
+    print(exps)
 
     return 200
 
@@ -19,6 +26,7 @@ def print_C(n):
 
 def main():
     print_C(10)
+    print_C(100)
 
 
 if __name__ == "__main__":
