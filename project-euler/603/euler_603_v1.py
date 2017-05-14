bd7cae5745afefb12f1796c8fe4557bdd94ec9e8
Shlomi Fish <shlomif@shlomifish.org>
2017-05-14 14:41:24 +0300

    Add #603.

---
+++ project-euler/603/euler_603_v1.py
@@ -0,0 +1,25 @@
+import sys
+from functools import reduce
+from six import print_
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+
+MOD = 1000000007
+
+def brute_S(n_num):
+    ret = long(0)
+    n = str(n_num)
+    for start in xrange(len(n)):
+        for end in xrange(start,len(n)):
+            ret += long(n[start:end+1])
+    return ret % MOD
+
+def main():
+    print brute_S(2024)
+
+
+if __name__ == "__main__":
+    main()
