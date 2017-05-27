67cfdf7e8410f27060835d84d42790063b20078c
Shlomi Fish <shlomif@shlomifish.org>
2017-05-27 17:52:04 +0300

    progress

--- project-euler/442/euler_442_v1.py
+++ project-euler/442/euler_442_v1.py
@@ -15,23 +15,34 @@ for i in xrange(0, 40):
 
 def calc_rev_func_ret(prefix, num_digs):
     if num_digs < 0:
-        raise BaseException("gloop")
+        raise BaseException("digits num is negative")
     ret = 0
-    tnumd = len(prefix) + num_digs
-    for d in xrange(0, 10):
-        np = prefix + str(d)
-        if np not in powers_str:
-            filtered = [x for x in powers_str
-                        if ((len(x) <= tnumd) and x.startswith(np))]
-            if len(filtered) == 0:
-                ret += rev_func_ret('', num_digs-1)
-            else:
-                ret += rev_func_ret(np, num_digs-1)
+    if num_digs == 0:
+        ret = (1 if (len([x for x in powers_str
+                          if (x in prefix)]) == 0) else 0)
+        # print "ret =", ret
+    else:
+        tnumd = len(prefix) + num_digs
+        for d in xrange(0, 10):
+            np = prefix + str(d)
+            # print "np =", np
+            if np not in powers_str:
+                filtered = [x for x in powers_str
+                            if ((len(x) <= tnumd) and x.startswith(np))]
+                if len(filtered) == 0:
+                    if num_digs == 1:
+                        ret += 1
+                    else:
+                        ret += rev_func_ret('', num_digs-1)
+                else:
+                    ret += rev_func_ret(np, num_digs-1)
+                # print "qpo =", np, num_digs, ret
+    # print "ret =", ret
     return ret
     # b 26 , num_digs == 0
 
 
-rev_func_cache = {('', 0): 0, ('', 1): 9}
+rev_func_cache = {('', 0): 1, ('', 1): 10}
 for d in xrange(1, 10):
     rev_func_cache[(str(d), 0)] = 1
 
@@ -49,19 +60,23 @@ def rev_func_ret(prefix, num_digs):
 
 
 def n_rev_func(n):
-    s = str(n+1)
+    s = str(n)
     ret = 0
     l = len(s)
     ns = ''
     for i, c in enumerate(s):
         lns = ns + str(c)
-        for d in xrange(0, 10):
+        for d in xrange((0 if i > l-1 or l > 0 else 1), 10):
             nns = ns + str(d)
             if nns == lns:
                 break
-            ret += rev_func_ret(nns, l-len(nns))
+            # print "nns=", nns
+            d = rev_func_ret(nns, l-len(nns))
+            # print "d=", d
+            ret += d
         ns = lns
-    return ret
+    ret += rev_func_ret(s, 0)
+    return ret - 1
 
 
 def assert_T(r, want):
@@ -74,6 +89,14 @@ def assert_T(r, want):
 
 def main():
     assert_T(3, 3)
+    assert_T(9, 9)
+    assert_T(11, 10)
+    assert_T(20, 19)
+    assert_T(21, 20)
+    assert_T(99, 98)
+    assert_T(100, 99)
+    assert_T(213, 200)
+    # assert_T(531563, 500000)
 
 
 if __name__ == "__main__":
