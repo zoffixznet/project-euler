f0320e863ac1302fde116582c7a92e63d2330dd7
Shlomi Fish <shlomif@shlomifish.org>
2017-05-27 18:28:09 +0300

    fix bugs

--- project-euler/442/euler_442_v1.py
+++ project-euler/442/euler_442_v1.py
@@ -17,18 +17,24 @@ def calc_rev_func_ret(prefix, num_digs):
     if num_digs < 0:
         raise BaseException("digits num is negative")
     ret = 0
-    if num_digs == 0:
-        ret = (1 if (len([x for x in powers_str
-                          if (x in prefix)]) == 0) else 0)
+    if (len([x for x in powers_str if (x in prefix)]) > 0):
+        ret = 0
+    elif num_digs == 0:
+        ret = 1
         # print "ret =", ret
     else:
         tnumd = len(prefix) + num_digs
         for d in xrange(0, 10):
             np = prefix + str(d)
-            # print "np =", np
+            # print "np =", prefix, np
             if np not in powers_str:
-                filtered = [x for x in powers_str
-                            if ((len(x) <= tnumd) and x.startswith(np))]
+                filtered = []
+                subp = ''
+                for c in np[::-1]:
+                    subp = str(c) + subp
+                    filtered += [x for x in powers_str
+                                 if ((len(x) <= tnumd - len(subp) + len(np))
+                                     and x.startswith(subp))]
                 if len(filtered) == 0:
                     if num_digs == 1:
                         ret += 1
@@ -36,7 +42,7 @@ def calc_rev_func_ret(prefix, num_digs):
                         ret += rev_func_ret('', num_digs-1)
                 else:
                     ret += rev_func_ret(np, num_digs-1)
-                # print "qpo =", np, num_digs, ret
+                # print "qpo =", np, num_digs, ret, filtered
     # print "ret =", ret
     return ret
     # b 26 , num_digs == 0
@@ -84,10 +90,23 @@ def assert_T(r, want):
     print("T(%d) = %d vs %d" % (r, got, want))
     if got != want:
         raise BaseException("fooblead")
+
+
+def check_T(n):
+    want = 0
+    for i in xrange(1, n+1):
+        got = n_rev_func(i)
+        s = str(i)
+        if not [x for x in powers_str if (x in s)]:
+            want += 1
+        print("checkT(%d) = %d vs %d" % (i, got, want))
+        if got != want:
+            raise BaseException("wrong result")
     return
 
 
 def main():
+    assert_T(120, 109)
     assert_T(3, 3)
     assert_T(9, 9)
     assert_T(11, 10)
@@ -96,7 +115,8 @@ def main():
     assert_T(99, 98)
     assert_T(100, 99)
     assert_T(213, 200)
-    # assert_T(531563, 500000)
+    assert_T(531563, 500000)
+    check_T(531563)
 
 
 if __name__ == "__main__":
