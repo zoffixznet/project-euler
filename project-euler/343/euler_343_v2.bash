1e87bd4d3f6b200f567a53e2e4f0f5927fddbda5
Shlomi Fish <shlomif@shlomifish.org>
2017-07-12 23:06:51 +0300

    Less excessive output.
    Only every 1,000 lines.

--- project-euler/343/euler_343_v2.bash
+++ project-euler/343/euler_343_v2.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 perl -Mbigint -E 'for (2 .. 2000000 ) { say $_*$_*$_+1; }' | factor | \
-    perl -Mbigint -lanE 'BEGIN {$s = 1;}; say $., "\t", ($s += $F[-1]-1); END{say $s}'
+    perl -Mbigint -lanE 'BEGIN {$s = 1;}; $s += $F[-1]-1;say $., "\t", $s if $. % 1_000 == 0 ; END{say $s}'
