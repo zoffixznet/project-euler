a255fd744ce71f078794a0be746ff5276c09a4f0
Shlomi Fish <shlomif@shlomifish.org>
2017-10-08 09:38:14 +0300

    no 611 - solve the example test cases.

---
+++ project-euler/611/euler_611_v1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import heapq
+import sys
+from six import print_
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+
+def solve_for(n):
+    def gen_sq():
+        sq = 1
+        d1, d2 = 3, 5
+        sq2 = sq + d1
+        sum_sq = sq + sq2
+        while sum_sq <= n:
+            yield (sum_sq, d2)
+            sq, sq2 = sq2, sq2 + d2
+            sum_sq = sq + sq2
+            d2 += 2
+        yield (n+1, n+1)
+    gen = gen_sq()
+    pq = []
+    heapq.heappush(pq, next(gen))
+    last = -1
+    count = 0
+    ret = 0
+    pivot_sq, pivot_d = next(gen)
+    while len(pq) > 0:
+        while pivot_sq <= n and pivot_sq <= pq[0][0]:
+            heapq.heappush(pq, (pivot_sq, pivot_d))
+            pivot_sq, pivot_d = next(gen)
+        this, d = heapq.heappop(pq)
+        if this != last:
+            if count == 1:
+                ret += 1
+            count = 0
+            last = this
+        count ^= 1
+        new = this+d
+        if new <= n:
+            heapq.heappush(pq, (new, d+2))
+    if count == 1:
+        ret += 1
+    return ret
+
+
+def p(n):
+    print_("F(%d) = %d" % (n, solve_for(n)))
+
+
+def main():
+    p(5)
+    p(100)
+    p(1000)
+    p(1000000)
+
+main()
