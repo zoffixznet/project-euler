2423fb968207c47d9d95a00f5c215ec0f8b5efcf
Shlomi Fish <shlomif@shlomifish.org>
2017-10-10 17:04:44 +0300

    Second version - not much faster.

---
+++ project-euler/611/euler_611_v2.cpp
@@ -0,0 +1,84 @@
+#include <functional>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <utility>
+
+typedef unsigned long long ll;
+typedef std::pair<ll, ll> p;
+
+ll solve_for(const ll n)
+{
+    ll sq = 1;
+    ll d2 = 5;
+    ll sq2 = 4;
+    std::priority_queue<p, std::vector<p>, std::greater<p>> pq;
+    ll sum_sq = sq + sq2;
+    while (sum_sq <= n)
+    {
+        pq.push(p(sum_sq, d2));
+        sq = sq2;
+        sq2 += d2;
+        sum_sq = sq + sq2;
+        d2 += 2;
+    }
+    ll ret = 0;
+    const auto l = pq.top();
+    pq.pop();
+    ll last = l.first;
+    ll new__ = last+l.second;
+    pq.push(p(new__, l.second+2));
+    while (true)
+    {
+        ll count = 1;
+        while (true)
+        {
+            if (pq.empty())
+            {
+                return (ret + (count&1));
+            }
+
+            const auto pa = pq.top();
+            pq.pop();
+            const auto this_ = pa.first;
+            const auto d = pa.second;
+            const auto new_ = this_+d;
+            if (new_ <= n)
+            {
+                pq.push(p(new_, d+2));
+            }
+            if (this_ != last)
+            {
+#if 0
+                std::cout << last << ' ' << count << std::endl;
+#endif
+                if (count & 1)
+                {
+                    if (((++ret) & 0x3FFFF) == 0)
+                    {
+                        std::cout << "Reached " << this_ << " d = " << d << " ret = " << ret << std::endl << std::flush;
+                    }
+                }
+                last = this_;
+                break;
+            }
+            ++count;
+        }
+    }
+}
+
+void pr(const ll n)
+{
+    const auto res = solve_for(n);
+    std::cout << "F(" << n << ") = " << res << std::endl;
+}
+
+int main()
+{
+    pr(5);
+    pr(100);
+    pr(1000);
+    pr(1000000);
+    pr(1000000000000LL);
+    return 0;
+}
