fbb8f6ad8b89e7668891c551ee2a7605cabc5afa
Shlomi Fish <shlomif@shlomifish.org>
2017-10-29 20:25:12 +0200

    unroll the recursion so it wont crash pypy.
    It yielded the correct solution. We are using a dedicated stack.

--- project-euler/284/euler_284_v1.py
+++ project-euler/284/euler_284_v1.py
@@ -20,17 +20,29 @@ MAX = 9
 
 def rec(n, sq, is_z, digits_sum):
     # print_('sq =', sq)
-    print_('n =', n)
-    if n > MAX:
-        return 0
-    if (((sq*sq) % powers[n]) != sq):
-        return 0
-    if sq == 0 and n > 3:
-        return 0
-    ret = 0 if is_z else digits_sum
-    m = MP[n]
-    for d in xrange(0, BASE):
-        ret += rec(n+1, sq+m[d], (d == 0), digits_sum+d)
+    s = [[sq, is_z, digits_sum, 0]]
+    ret = 0
+    while len(s):
+        if len(s) & 0xFF == 0:
+            print_('n =', len(s))
+        if len(s) > MAX+1:
+            s.pop()
+            continue
+        sq, is_z, digits_sum, d = s[-1]
+        if (((sq*sq) % powers[len(s)-1]) != sq):
+            s.pop()
+            continue
+        if sq == 0 and len(s) > 3:
+            s.pop()
+            continue
+        if d == 0:
+            if not is_z:
+                ret += digits_sum
+        elif d == BASE:
+            s.pop()
+            continue
+        s[-1][-1] += 1
+        s.append([sq+MP[len(s)-1][d], (d == 0), digits_sum+d, 0])
     return ret
 
 print_(rec(0, long(0), True, 0))
