6e1ba9b787ffb928aab95cb5982e5ad66b59bbec
Shlomi Fish <shlomif@shlomifish.org>
2017-10-30 10:32:39 +0200

    tweak the algo

--- project-euler/284/euler_284_v1.py
+++ project-euler/284/euler_284_v1.py
@@ -16,24 +16,34 @@ for n in xrange(1, 10001):
 MP = [[p * d for d in xrange(0, BASE)] for p in powers]
 
 MAX = 9
+ret = 0
 
 
 def rec(n, sq, is_z, digits_sum):
+    global ret
     # print_('sq =', sq)
     if n & 0xFF == 0:
         print_('n =', n)
     if n > MAX:
-        return 0
+        return
     if (((sq*sq) % powers[n]) != sq):
-        return 0
+        return
     if sq == 0 and n > 3:
-        return 0
-    ret = 0 if is_z else digits_sum
+        return
+    if not is_z:
+        ret += digits_sum
     m = MP[n]
     for d in xrange(0, BASE):
-        ret += rec(n+1, sq+m[d], (d == 0), digits_sum+d)
+        rec(n+1, sq+m[d], (d == 0), digits_sum+d)
+
+
+def wrap(max_):
+    global MAX, ret
+    ret = 0
+    MAX = max_
+    rec(0, long(0), True, 0)
     return ret
 
-print_(rec(0, long(0), True, 0))
-MAX = 10000
-print_(rec(0, long(0), True, 0))
+
+print_(wrap(9))
+print_(wrap(10000))
