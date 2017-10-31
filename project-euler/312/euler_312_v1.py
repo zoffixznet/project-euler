abcdd17f89415aa3f7392ca6a36583d9d78ba679
Shlomi Fish <shlomif@shlomifish.org>
2017-10-31 17:01:17 +0200

    Solved.

--- project-euler/312/euler_312_v1.py
+++ project-euler/312/euler_312_v1.py
@@ -15,9 +15,10 @@ def paths_count(n, MOD):
         return (1, 1)
     if n == 2:
         return (2, 3)
-    p, d = paths_count(n-1, MOD)
-    m = ((p*d) << 1)
-    return ((m * p) % MOD, (m * d) % MOD)
+    pd = paths_count(2, MOD)
+    for i in xrange(3, n+1):
+        pd = step(pd, MOD)
+    return pd
 
 
 def C(n, MOD):
@@ -31,25 +32,10 @@ class FooTestCase(unittest.TestCase):
         self.assertEqual(C(3, 10**100), 8)
         self.assertEqual(C(5, 10**100), 71328803586048)
         self.assertEqual(C(10000, 13 ** 8), 617720485)
+        self.assertEqual(C(10000, 10 ** 8), 37652224)
 
 
-f = {}
-n = 1
-
-
-def proc(arg):
-    global f, n
-    if arg in f:
-        print_('found n = ', n, ' prev = ', f[arg])
-        raise BaseException('f')
-    f[arg] = n
-    n += 1
-
-
-MOD = 13 ** 8
-
-
-def step(pd):
+def step(pd, MOD):
     p, d = pd
     m = ((p*d) << 1)
     return ((m * p) % MOD, (m * d) % MOD)
@@ -59,30 +45,44 @@ def main():
     # unittest.main()
     pd = (1, 1)
     pd = (2, 3)
-    # init_pd = pd = step(pd)
+    # init_pd = pd = step(pd, MOD)
+    MOD = 13 ** 8
     init_pd = pd
-    global n
     # n = 3
     n = 2
-    if False:
+    if True:
         while True:
-            pd = step(pd)
+            pd = step(pd, MOD)
             n += 1
             if init_pd == pd:
                 print_('Found ', n)
+                break
 
     # C(2) % 13 ** 8 = C(28960856)
-    global MOD
     MOD = 28960856 - 2
-    if False:
-        init_pd = pd = step(pd)
+    if True:
+        pd = (2, 3)
+        n = 2
+        init_pd = pd = step(pd, MOD)
         n += 1
         while True:
-            pd = step(pd)
+            pd = step(pd, MOD)
             n += 1
             if init_pd == pd:
                 print_('Found ', n)
+                break
+
+    def off_C(offset, n, MOD):
+        # return C(n, MOD)
+        offset = 0
+        return offset + C(n - offset, MOD)
     # C(3) % 28960854 = C(171369) % 28960854
+    C1 = off_C(3, 10000, 171369-3)
+    print_(C1)
+    C2 = off_C(2, C1, 28960854)
+    print_(C2)
+    C3 = C(C2, 13 ** 8)
+    print_(C3)
 
 
 main()
