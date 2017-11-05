e8b846e782b6906ad490fd2fb4280b14fedd8337
Shlomi Fish <shlomif@shlomifish.org>
2017-11-05 17:02:39 +0200

    Add more.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ mathematical problems that may be solved by writing computer programs.
 Note that some of these solutions were copied from elsewhere on the Internet.
 
 The licences of the contents of this repository varies, but most of the
-original content is available under the MIT/X11 License.
+original content is available under the MIT/Expat License.
 
 As usual, all the code here comes as is, and WITHOUT WARRANTY.
 
@@ -14,4 +14,4 @@ As usual, all the code here comes as is, and WITHOUT WARRANTY.
 
 * In the [Perl 6 Examples](https://github.com/perl6/perl6-examples/tree/master/categories/euler) repository.
 * [Haskell Wiki](https://wiki.haskell.org/Euler_problems) solutions in Haskell.
-
+* [Project Euler solutions in Python, Haskell, Ruby, Clojure, Go, and Scheme.](https://github.com/zacharydenton/euler) - by Zachary Denton and others.
--- project-euler/613/euler_613_v1.py
+++ project-euler/613/euler_613_v1.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python
 
 import sys
-from bigfloat import BigFloat, precision, acos, atan2, sqrt
+from math import acos, atan2, sqrt
 from six import print_
 
 if sys.version_info > (3,):
     long = int
     xrange = range
 
-with precision(100):
-    FROM_RAD = 1/(2*acos(-1))
+FROM_RAD = 1/(2*acos(-1))
 
-    def calc(num_steps, H, W):
-        H2 = H*H
-        ret = BigFloat('0')
-        count = 0
-        WSTEP = W / num_steps
-        # for y in xrange(1, num_steps+1):
-        xx = BigFloat('0')
-        while xx < W:
-            count += 1
-            ret += (atan2(xx, H) * sqrt(H2+xx*xx))
+def calc(num_steps, H, W):
+    H2 = H*H
+    ret = 0.0
+    count = 0
+    WSTEP = W / num_steps
+    # for y in xrange(1, num_steps+1):
+    xx = 0.0
+    while xx < W:
+        count += 1
+        ret += (atan2(xx, H) * sqrt(H2+xx*xx))
 
-            xx += WSTEP
-        return ret * FROM_RAD / count / H
+        xx += WSTEP
+    return ret * FROM_RAD / count / H
 
-    log = 1
-    num_steps = 2
-    H = BigFloat('30')
-    W = BigFloat('40')
-    while True:
-        res = calc(num_steps, H, W) + calc(num_steps, W, H)
-        print_("%8d : %.60f" % (log, res))
-        log += 1
-        num_steps <<= 1
+log = 1
+num_steps = 2
+H = 30.0
+W = 40.0
+while True:
+    res = calc(num_steps, H, W) + calc(num_steps, W, H)
+    print_("%8d : %.60f" % (log, res))
+    log += 1
+    num_steps <<= 1
