b92e1e55769bcf1849b832306ded11279d5689cb
Shlomi Fish <shlomif@shlomifish.org>
2017-11-07 18:05:55 +0200

    Remove old cruft; refactoring.

--- project-euler/607/Euler607.pm
+++ project-euler/607/Euler607.pm
@@ -1,192 +1,5 @@
-package Euler607::Seg;
-
 use strict;
 use warnings;
-
-use Carp::Always;
-
-use parent 'Exporter';
-
-our $TYPE_X_ONLY = 0;
-our $TYPE_XY     = 1;
-
-our @EXPORT_OK =
-    qw($TYPE_X_ONLY $TYPE_XY compile_segment intersect intersect_x);
-
-sub gcd
-{
-    my ( $n, $m ) = @_;
-
-    if ( $m > $n )
-    {
-        ( $n, $m ) = ( $m, $n );
-    }
-
-    while ( $m > 0 )
-    {
-        ( $n, $m ) = ( $m, $n % $m );
-    }
-
-    return $n;
-}
-
-sub signed_gcd
-{
-    my ( $n, $m ) = @_;
-
-    return gcd( abs($n), abs($m) );
-}
-
-sub _reduce
-{
-    my ( $n, $d ) = @_;
-
-    my $g = signed_gcd( $n, $d );
-
-    my @ret = ( $n / $g, $d / $g );
-
-    if ( $ret[0] == 0 )
-    {
-        return ( 0, 1 );
-    }
-    elsif ( $ret[1] < 0 )
-    {
-        return map { -$_ } @ret;
-    }
-    else
-    {
-        return @ret;
-    }
-}
-
-sub _mul
-{
-    my ( $x, $y ) = @_;
-
-    return $x * $y;
-}
-
-sub _div
-{
-    my ( $x, $y ) = @_;
-
-    return _mul( $x, 1 / $y );
-}
-
-sub _add
-{
-    my ( $x, $y ) = @_;
-
-    return $x + $y;
-}
-
-sub _subtract
-{
-    my ( $x, $y ) = @_;
-
-    return _add( $x, -$y );
-}
-
-sub _lt
-{
-    my ( $x, $y ) = @_;
-
-    return ( $x->[0] * $y->[1] < $x->[1] * $y->[0] );
-}
-
-sub _lt2
-{
-    my ( $f, $x ) = @_;
-
-    return ( $f->[0] < $x * $f->[1] );
-}
-
-sub _lt3
-{
-    my ( $x, $f ) = @_;
-
-    return ( $f->[0] > $x * $f->[1] );
-}
-
-sub _eq
-{
-    my ( $x, $y ) = @_;
-
-    return ( $x == $y );
-}
-
-sub _f
-{
-    return [ shift, 1 ];
-}
-
-sub compile_segment
-{
-    my ($L) = @_;
-
-    my ( $x1, $y1, $x2, $y2 ) = @$L;
-    my @y_s = sort { $a <=> $b } ( $y1, $y2 );
-
-    if ( $x1 == $x2 )
-    {
-        if ( $y1 == $y2 )
-        {
-            die "Duplicate point in segment [@$L].";
-        }
-        return {
-            t  => $TYPE_X_ONLY,
-            x  => $x1,
-            X  => _f($x1),
-            y1 => $y_s[0],
-            y2 => $y_s[-1],
-        };
-    }
-    else
-    {
-        my $m = [ _reduce( $y2 - $y1, $x2 - $x1 ) ];
-        my $bb = _subtract( _f($y1), _mul( $m, _f($x1) ) );
-        my @x_s = sort { $a <=> $b } ( $x1, $x2 );
-        return {
-            t  => $TYPE_XY,
-            m  => $m,
-            b  => $bb,
-            x1 => $x_s[0],
-            x2 => $x_s[-1],
-            y1 => $y_s[0],
-            y2 => $y_s[-1],
-        };
-    }
-}
-
-sub intersect_x
-{
-    my ( $s1, $s2 ) = @_;
-    my ( $X,  $x )  = @$s1{qw(X x)};
-
-    my $y = _add( _mul( $s2->{'m'}, $X ), $s2->{'b'} );
-
-    return [ $X, $y ];
-}
-
-sub intersect
-{
-    my ( $s1, $s2 ) = @_;
-
-    # Both are y = f(x) so m1x+b1 == m2x+b2 ==> x
-    if ( _eq( $s1->{'m'}, $s2->{'m'} ) )
-    {
-        return;
-    }
-    else
-    {
-        my $x = _div(
-            _subtract( $s2->{'b'}, $s1->{'b'} ),
-            _subtract( $s1->{'m'}, $s2->{'m'} )
-        );
-
-        return [ $x, _add( $s2->{'b'}, _mul( $s2->{'m'}, $x ) ) ];
-    }
-}
 
 package MySvg;
 
@@ -303,6 +116,23 @@ sub snell
     return $deg2;
 }
 
+sub intersect
+{
+    my ( $s1, $s2 ) = @_;
+
+    # Both are y = f(x) so m1x+b1 == m2x+b2 ==> x
+    if ( $s1->{'m'} == $s2->{'m'} )
+    {
+        return;
+    }
+    else
+    {
+        my $x = ( ( $s2->{'b'} - $s1->{'b'} ) / ( $s1->{'m'} - $s2->{'m'} ) );
+
+        return [ $x, $s2->{'b'} + $s2->{'m'} * $x ];
+    }
+}
+
 sub solve
 {
     my ( $deg, $svg ) = @_;
@@ -313,9 +143,7 @@ sub solve
     my $m1 = tan($deg);
     my $m0 = 1;
     my $b1 = -( 100 - 50 * sqrt(2) ) / 2;
-    my $p2 =
-        Euler607::Seg::intersect( { m => $m1, b => 0 },
-        { m => $m0, b => $b1 } );
+    my $p2 = intersect( { m => $m1, b => 0 }, { m => $m0, b => $b1 } );
 
     my $distance = 0;
     $distance += dist( $p, $p2 ) / $v1;
@@ -333,8 +161,7 @@ sub solve
 
         my $m2 = tan($deg2);
         $b -= 10 * sqrt(2);
-        my $p3 = Euler607::Seg::intersect(
-            { m => $m2, b => ( $p2->[1] - $m2 * $p2->[0] ) },
+        my $p3 = intersect( { m => $m2, b => ( $p2->[1] - $m2 * $p2->[0] ) },
             { m => $m0, b => $b } );
 
         $svg->line( @$p2, @$p3, shift @colors );
@@ -350,7 +177,7 @@ sub solve
 
     my $p6     = $ctx->{p};
     my $x8     = 100;
-    my $dest_p = Euler607::Seg::intersect(
+    my $dest_p = intersect(
         { m => $m1, b => ( $p6->[1] - $m1 * $p6->[0] ) },
         { m => $m0, b => ( 0 - $m0 * $x8 ) },
     );
