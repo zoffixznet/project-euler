fde053efd56b7eb2b0855c70e7f67ebb6640e0a3
Shlomi Fish <shlomif@shlomifish.org>
2017-11-13 17:55:46 +0200

    add more

--- project-euler/433/euler_433_v1.py
+++ project-euler/433/euler_433_v1.py
@@ -2,19 +2,39 @@
 
 import sys
 from six import print_
+# import functools
 
 if sys.version_info > (3,):
     long = int
     xrange = range
 
+c = [[0], [1]]
+
+
+def E(x, y):
+    if y <= 1:
+        return y
+    if y <= 30000:
+        return c[y][x % y]
+    return 1 + E(y, x % y)
+
+
+for y in xrange(2, 30001):
+    c.append([])
+    arr = c[y]
+    for m in xrange(y):
+        arr.append(E(y, m)+1)
+
 
 # @functools.lru_cache(maxsize=128*1024)
-def E(x, y):
-    r = 0
-    while y != 0:
-        r += 1
-        x, y = y, x % y
-    return r
+def R(x, y):
+    return 1+G(y, x % y)
+
+
+def G(x, y):
+    if y <= 1:
+        return y
+    return R(x, y)
 
 
 def S(n):
@@ -23,14 +43,12 @@ def S(n):
     ret += n
     for y in xrange(1, n+1):
         s = m = n % y
-        for x in xrange(1, m):
+        for x in xrange(1, m+1):
             s += E(y, x)
-        s += E(y, m)
         ss = s
-        s += y - m
         for x in xrange(m+1, y):
             s += E(y, x)
-        ret += ((ss + s * ((n - y) // y)) << 1) + (n-y)
+        ret += ((ss + (s + y - m) * ((n - y) // y)) << 1) + (n-y)
         if ((y & 0x3FF) == 0):
             print_("y = %d ; ret = %d" % (y, ret))
             sys.stdout.flush()
