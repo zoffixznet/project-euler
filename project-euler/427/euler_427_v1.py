6e3a13b778f0a5dbc872a89b8413ef4453f46122
Shlomi Fish <shlomif@shlomifish.org>
2017-11-17 20:10:21 +0200

    Add solution to #427.
    Kinda slow.

---
+++ project-euler/427/euler_427_v1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import sys
+from six import print_
+from collections import deque
+
+if sys.version_info > (3,):
+    long = int
+    xrange = range
+
+
+def count(n, p):
+    if p > n:
+        return 0
+    return n // p + count(n // p, p)
+
+
+def main():
+    MOD = 1000000009
+
+    def expmod(b, e):
+        if e == 0:
+            return 1
+        r = 1
+        if ((e & 1) == 1):
+            r *= b
+        rec = expmod(b, e >> 1)
+        return ((r * rec * rec) % MOD)
+
+    def f(n):
+        at_most = [0 for i in xrange(0, n+1)]
+        nm = n-1
+        at_most[1] = (n * expmod(nm, nm)) % MOD
+        at_most[n] = expmod(n, n)
+
+        for len_m in xrange(2, n):
+            len_ = n+1-len_m
+            state = deque([n])
+            s = n
+            for pos in xrange(2, len_+1):
+                state.append(s * nm % MOD)
+                s = ((s * n) % MOD)
+            for pos in xrange(len_+1, n+1):
+                state.append(s * nm % MOD)
+                s = ((s * n - state.popleft()) % MOD)
+                # print_('p = %d ; pos_ = %d' % (len_, pos))
+            at_most[len_] = s
+            print_('p = %d ; ret = %d' % (len_, at_most[len_]))
+            sys.stdout.flush()
+        only = [0 for i in xrange(0, n+1)]
+        for len_ in xrange(1, n+1):
+            only[len_] = at_most[len_] - at_most[len_-1]
+        assert only[n] == n
+        assert only[1] % MOD == at_most[1]
+
+        ret = sum([x*i for i, x in enumerate(only)]) % MOD
+        print_('ret = %d' % (ret))
+        return ret
+
+    assert f(3) == 45
+    assert f(7) == 1403689
+    assert f(11) == 481496895121 % MOD
+    print_(f(7500000))
+
+main()
