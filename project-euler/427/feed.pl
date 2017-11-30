324993e96a19ff0e10e903d69f1038c10f859a0f
Shlomi Fish <shlomif@shlomifish.org>
2017-11-30 17:35:16 +0200

    fix tidy

--- project-euler/427/feed.pl
+++ project-euler/427/feed.pl
@@ -7,13 +7,13 @@ my $i = 3199844;
 
 my $FIN = 2;
 
-while ($i >= $FIN)
+while ( $i >= $FIN )
 {
     my $e = $i - 100;
-    if ($e < $FIN)
+    if ( $e < $FIN )
     {
         $e = $FIN;
     }
     print "${e}_${i}\n";
-    $i = $e-1;
+    $i = $e - 1;
 }
