44a96c4f1807d896c63c68ac8178d2009cc1d129
Shlomi Fish <shlomif@shlomifish.org>
2017-12-07 17:36:59 +0200

    Faster, but wrong.

--- project-euler/613/euler_613_v1.py
+++ project-euler/613/euler_613_v1.py
@@ -11,27 +11,39 @@ if sys.version_info > (3,):
 FROM_RAD = 1/(2*acos(-1))
 
 
-def calc(num_steps, H, W):
+def calc(H, W):
     H2 = H*H
     ret = 0.0
     count = 0
+    num_steps = 2
     WSTEP = W / num_steps
-    # for y in xrange(1, num_steps+1):
-    xx = 0.0
-    while xx < W:
-        count += 1
-        ret += (atan2(xx, H) * sqrt(H2+xx*xx))
+    WS = WSTEP * 2
+    count += 1
+    xx = 0
+    ret += (atan2(xx, H) * sqrt(H2+xx*xx))
+    count += 1
+    xx = W
+    ret += (atan2(xx, H) * sqrt(H2+xx*xx))
+    while True:
+        # for y in xrange(1, num_steps+1):
+        xx = WSTEP
+        while xx < W:
+            count += 1
+            ret += (atan2(xx, H) * sqrt(H2+xx*xx))
 
-        xx += WSTEP
-    return ret * FROM_RAD / count / H
+            xx += WS
+        yield ret * FROM_RAD / count / H
+        num_steps <<= 1
+        WSTEP *= 0.5
+        WS = WSTEP * 2
 
 
 log = 1
-num_steps = 2
 H = 30.0
 W = 40.0
+gens = [calc(H, W), calc(W, H)]
 while True:
-    res = calc(num_steps, H, W) + calc(num_steps, W, H)
-    print_("%8d : %.60f" % (log, res))
+    res = sum([next(x) for x in gens])
+    print_("%8d[a] : %.50f" % (log, res))
+    print_("%8d[b] : %.50f" % (log, 1 - res))
     log += 1
-    num_steps <<= 1
