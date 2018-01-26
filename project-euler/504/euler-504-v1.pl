2461e1472fcb667a061d28c437f18cf2df1f6b0d
Shlomi Fish <shlomif@shlomifish.org>
2018-01-26 15:27:19 +0200

    add licence

--- project-euler/504/euler-504-v1.pl
+++ project-euler/504/euler-504-v1.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+# Copyright by Shlomi Fish, 2018 under the Expat licence
+# https://opensource.org/licenses/mit-license.php
+
 use strict;
 use warnings;
 
@@ -9,3 +12,4 @@ use Euler504;
 Euler504::set_M(100);
 
 print "Answer = ", Euler504::calc_all_quadris(), "\n";
+
