a8df09f995e1abc8f0a87e6c6c811fc6d180791c
Shlomi Fish <shlomif@shlomifish.org>
2018-01-26 14:05:27 +0200

    Add licence.

--- project-euler/1/1.rb
+++ project-euler/1/1.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/ruby
 
+# Copyright by Shlomi Fish, 2018 under the Expat licence
+# https://opensource.org/licenses/mit-license.php
+
 sum = 0
 ((1...1000).select {|i| (((i % 3) == 0) || ((i % 5) == 0)) }) \
     .each {|i| sum += i}
