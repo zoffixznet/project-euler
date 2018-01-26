0a9180dd60dc9336f7d578d6a4255c44a1666dcc
Shlomi Fish <shlomif@shlomifish.org>
2018-01-26 14:04:05 +0200

    Reformat.

--- project-euler/1/1.rb
+++ project-euler/1/1.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 
 sum = 0
-((1...1000).select {|i| (((i % 3) == 0) || ((i%5) == 0)) }).each {|i| sum += i}
+((1...1000).select {|i| (((i % 3) == 0) || ((i % 5) == 0)) }) \
+    .each {|i| sum += i}
 puts sum
