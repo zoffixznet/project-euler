148188275f4aae466e5a319f77e2108e2273d5c8
Shlomi Fish <shlomif@shlomifish.org>
2018-02-05 16:09:16 +0200

    Got rid of floats and other ops.

--- project-euler/368/euler_368_v1.py
+++ project-euler/368/euler_368_v1.py
@@ -25,28 +25,31 @@
 
 import sys
 import re
-from bigfloat import precision
 
 if sys.version_info > (3,):
     long = int
 
-with precision(60):
+
+def main():
     r = re.compile('(000|111|222|333|444|555|666|777|888|999)')
     n = 1
     # total = BigFloat('0')
-    total = 0.0
+    total = 0
     step = 100000
     checkpoint = step
     while True:
         n_s = str(n)
         m = r.search(n_s)
         if m:
-            n += int('1' + '0' * (len(n_s) - m.end(1)))
+            n += 10 ** (len(n_s) - m.end(1))
         else:
             # total += BigFloat('1') / n
-            total += 1.0 / n
+            total += 1000000000000000000000000000000 / n
             while n > checkpoint:
-                print(("n=%030d t = %.40f") % (n, total))
+                print(("n=%030d t = %d") % (n, total))
                 sys.stdout.flush()
                 checkpoint += step
             n += 1
+
+
+main()
